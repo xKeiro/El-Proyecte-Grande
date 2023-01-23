a18b0f7b65c37d3600843ddfff1a726b0413ddfb
xKeiro <nkevin@outlook.hu>
2023-01-24 00:55:50 +0100

    Add initial Ingredient model

---
+++ ElProyecteGrande/Models/Ingredient.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElProyecteGrande.Models;
+
+public class Ingredient: BaseModel
+{
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(60, MinimumLength = 2)]
+    public required string Name { get; set; }
+}
