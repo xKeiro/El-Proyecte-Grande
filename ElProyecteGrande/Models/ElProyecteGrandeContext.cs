621ea073624c5a3c333b9c5a923f802fcd5e51a4
xKeiro <nkevin@outlook.hu>
2023-01-24 00:56:42 +0100

    Add context for database

---
+++ ElProyecteGrande/Models/ElProyecteGrandeContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ElProyecteGrande.Models;
+
+public class ElProyecteGrandeContext : DbContext
+{
+    public ElProyecteGrandeContext(DbContextOptions<ElProyecteGrandeContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Recipe> Recipes { get; set; } = default!;
+    public DbSet<Ingredient> Ingredients { get; set;} = default!;
+}
