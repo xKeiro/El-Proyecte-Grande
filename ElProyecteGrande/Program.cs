f49de8c7d1f6ee4f8c98c8d1ade8e96abca7fda7
xKeiro <nkevin@outlook.hu>
2023-01-24 00:59:39 +0100

    Add DbContext service to the main program

--- ElProyecteGrande/Program.cs
+++ ElProyecteGrande/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ElProyecteGrande.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<ElProyecteGrandeContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ElProyecteGrandeContext")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
