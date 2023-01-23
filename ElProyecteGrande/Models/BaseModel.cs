e1264af3e546d6bf85802da71c969042f9829607
xKeiro <nkevin@outlook.hu>
2023-01-24 00:55:13 +0100

    Add initial base class for models

---
+++ ElProyecteGrande/Models/BaseModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElProyecteGrande.Models;
+
+public abstract class BaseModel
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public virtual int Id { get; set; }
+}
