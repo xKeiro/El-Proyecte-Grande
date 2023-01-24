4d3333c512ce17c9ba10b80fe41f385148a3ccbc
xKeiro <nkevin@outlook.hu>
2023-01-24 15:28:48 +0100

    Update db context to handle enums and include modified models

---
+++ ElProyecteGrande/Services/ElProyecteGrandeContext.cs
@@ -0,0 +1,37 @@
+﻿using ElProyecteGrande.Models;
+using ElProyecteGrande.Models.Categories;
+using ElProyecteGrande.Models.Enums;
+using ElProyecteGrande.Models.Recipes;
+using ElProyecteGrande.Models.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace ElProyecteGrande.Services;
+
+public class ElProyecteGrandeContext : DbContext
+{
+    public ElProyecteGrandeContext(DbContextOptions<ElProyecteGrandeContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Recipe> Recipes { get; set; } = default!;
+    public DbSet<RecipeReview> RecipeReviews { get; set; } = default!;
+    public DbSet<RecipeIngredient> RecipeIngredients { get; set; } = default!;
+    public DbSet<Ingredient> Ingredients { get; set; } = default!;
+    public DbSet<User> Users { get; set; } = default!;
+    public DbSet<UserRecipe> UserRecipes { get; set; } = default!;
+    public DbSet<UserRecipeStatus> UserRecipeStatuses { get; set; } = default!;
+    public DbSet<Categorization> Categorizations { get; set; } = default!;
+    public DbSet<Cuisine> Cuisines { get; set; } = default!;
+    public DbSet<Diet> Diets { get; set; } = default!;
+    public DbSet<MealTime> MealTimes { get; set; } = default!;
+    public DbSet<DishType> DishTypes { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<UserRecipeStatus>()
+            .Property(userRecipeStatus => userRecipeStatus.Name)
+            .HasConversion(new EnumToStringConverter<RecipeStatus>());
+    }
+}
