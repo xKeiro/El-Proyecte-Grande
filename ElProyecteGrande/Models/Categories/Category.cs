c9b301384854e4d98a2a34e3476d7a9ce5f1a026
xKeiro <nkevin@outlook.hu>
2023-01-24 12:40:50 +0100

    Update Category to hold different type of categories and move to new namespace

---
+++ ElProyecteGrande/Models/Categories/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElProyecteGrande.Models.Categories;
+
+public class Category : BaseModel
+{
+    [Required]
+    public required Cuisine Cuisine { get; set; }
+    [Required]
+    public required List<Meal> Meals { get; set; }
+    [Required]
+    public required List<Diet> Diets { get; set; }
+    [Required]
+    public required MealType MealType { get; set; }
+}
