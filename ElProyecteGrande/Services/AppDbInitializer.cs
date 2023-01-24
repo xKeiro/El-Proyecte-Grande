c82b25ac19606c27ffe1264f8b1133e5c91786d2
Lilla Meszaros <ameszaroslilla@gmail.com>
2023-01-24 20:56:49 +0100

    Add initial data to ingredients and users

--- ElProyecteGrande/Services/AppDbInitializer.cs
+++ ElProyecteGrande/Services/AppDbInitializer.cs
@@ -1,4 +1,6 @@
-﻿using ElProyecteGrande.Models.Categories;
+﻿using ElProyecteGrande.Models;
+using ElProyecteGrande.Models.Categories;
+using ElProyecteGrande.Models.Users;
 
 namespace ElProyecteGrande.Services
 {
@@ -73,6 +75,70 @@ namespace ElProyecteGrande.Services
                     });
                     context.SaveChanges();
                 }
+
+                if (context.Ingredients.Any())
+                {
+                    return;
+                }
+
+                //Ingredients
+                Ingredient penne = new Ingredient { Name = "Penne Pasta", UnitOfMeasure = "oz" };
+                Ingredient tomato = new Ingredient { Name = "Tomatoes", UnitOfMeasure = "lb" };
+                Ingredient basil = new Ingredient { Name = "Basil", UnitOfMeasure = "g" };
+                Ingredient parmesan = new Ingredient { Name = "Parmesan", UnitOfMeasure = "oz" };
+                Ingredient chicken = new Ingredient { Name = "Chicken Breast", UnitOfMeasure = "lb" };
+                Ingredient romanianLettuce = new Ingredient
+                { Name = "Romaine Lettuce", UnitOfMeasure = "bunch" };
+                Ingredient flour = new Ingredient { Name = "Flour", UnitOfMeasure = "cup" };
+                Ingredient sugar = new Ingredient { Name = "Sugar", UnitOfMeasure = "cup" };
+                Ingredient eggs = new Ingredient { Name = "Eggs", UnitOfMeasure = "unit" };
+                Ingredient chocolateChip = new Ingredient { Name = "Chocolate Chip", UnitOfMeasure = "cup" };
+                context.Ingredients.AddRange(
+                    penne,
+                    tomato,
+                    basil,
+                    parmesan,
+                    chicken,
+                    romanianLettuce,
+                    flour,
+                    sugar,
+                    eggs,
+                    chocolateChip
+                );
+                context.SaveChanges();
+
+                //Users
+                if (context.Users.Any())
+                {
+                    return;
+                }
+                var user1 = new User
+                {
+                    Username = "FoodLover123",
+                    EmailAddress = "ilovefood@example.com",
+                    Password = "12345678",
+                    IsAdmin = false
+                };
+                var user2 = new User
+                {
+                    Username = "JaneDoe",
+                    EmailAddress = "jane@example.com",
+                    Password = "87654321",
+                    IsAdmin = false
+                };
+                context.Users.AddRange(user1, user2);
+                context.SaveChanges();
+
+                var user3 = new User
+                {
+                    Username = "MichaelSmit",
+                    EmailAddress = "smith@example.com",
+                    Password = "pw1234567",
+                    IsAdmin = false
+                };
+                context.Users.AddRange(user1, user2);
+                context.SaveChanges();
+
             }
         }
     }
