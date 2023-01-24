cd319119b6c1f213c8961d2836c0e2e587656c23
xKeiro <nkevin@outlook.hu>
2023-01-24 12:44:20 +0100

    Update Recipe model with Description, Category and RecipeIngredients. And move it to new namespace

---
+++ ElProyecteGrande/Models/Recipes/Recipe.cs
@@ -0,0 +1,21 @@
+﻿using ElProyecteGrande.Models.Categories;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElProyecteGrande.Models.Recipes;
+
+public class Recipe : BaseModel
+{
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(120, MinimumLength = 2)]
+    public required string Name { get; set; }
+
+    [Required]
+    public required string Description { get; set; }
+
+    [Required]
+    public required Category Category { get; set; }
+
+    [Required]
+    public required List<RecipeIngredient> RecipeIngredients { get; set; }
+
+}
