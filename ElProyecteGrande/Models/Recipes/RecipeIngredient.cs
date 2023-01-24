839b5547d75ca35cdefa25c1592cc397bb2a2f2c
xKeiro <nkevin@outlook.hu>
2023-01-24 12:42:27 +0100

    Add RecipeIngredient model

---
+++ ElProyecteGrande/Models/Recipes/RecipeIngredient.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Build.Framework;
+
+namespace ElProyecteGrande.Models.Recipes;
+
+public class RecipeIngredient : BaseModel
+{
+    [Required]
+    public required Recipe Recipe { get; set; }
+    [Required]
+    public required Ingredient Ingredient { get; set; }
+    [Required]
+    public required decimal Amount { get; set; }
+
+}
