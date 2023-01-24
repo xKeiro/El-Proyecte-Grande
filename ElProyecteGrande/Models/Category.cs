5cc7e6c38ef3462d5a33923770e6760563307d15
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-01-24 11:17:10 +0100

    Add User and Category models

---
+++ ElProyecteGrande/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using ElProyecteGrande.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElProyecteGrande.Models_Miki
+{
+    public class Category : BaseModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
+        public required string Name { get; set; }
+    }
+}
---
+++ ElProyecteGrande/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ElProyecteGrande.Models;
+
+namespace ElProyecteGrande.Models_Miki
+{
+    public class User : BaseModel
+    {
+        [Required]
+        public required string Username { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        [Required]
+        [RegularExpression("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$", ErrorMessage = "Invalid pattern")]
+        public required string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
+        
+        [Required]
+        public required bool IsAdmin { get; set; } = false;
+    }
+}
