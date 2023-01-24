b592f7048bc67705febeba6dabb125affd2b5b41
Lilla Meszaros <ameszaroslilla@gmail.com>
2023-01-24 07:17:12 +0100

    Add RecipeReview model

---
+++ ElProyecteGrande/Models/RecipeReview.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElProyecteGrande.Models
+{
+    public class RecipeReview : BaseModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public override int Id { get; set; }
+
+        [Required]
+        public User User { get; set; }
+
+        [Required]
+        public Recipe Recipe { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int Rate { get; set; }
+
+        public string? Review { get; set; }
+
+    }
+}
