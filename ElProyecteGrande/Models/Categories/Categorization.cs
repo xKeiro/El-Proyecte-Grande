134f318b37d308e90ccfed5c8da3d02fdfa14d74
xKeiro <nkevin@outlook.hu>
2023-01-24 15:22:27 +0100

    Refactor rename Category model to Categorization

--- ElProyecteGrande/Models/Categories/Categorization.cs
+++ ElProyecteGrande/Models/Categories/Categorization.cs
@@ -2,14 +2,14 @@
 
 namespace ElProyecteGrande.Models.Categories;
 
-public class Category : BaseModel
+public class Categorization : BaseModel
 {
     [Required]
     public required Cuisine Cuisine { get; set; }
     [Required]
-    public required List<Meal> Meals { get; set; }
+    public required List<MealTime> Meals { get; set; }
     [Required]
     public required List<Diet> Diets { get; set; }
     [Required]
-    public required MealType MealType { get; set; }
+    public required DishType DishType { get; set; }
 }
