a9709bf3f2a1d2b32ff0ae7fbf74128123c07a0e
Lilla Meszaros <ameszaroslilla@gmail.com>
2023-01-25 14:17:01 +0100

    Add RecipeController and add endpoint to get all Recipes

---
+++ ElProyecteGrande/Controllers/RecipeController.cs
@@ -0,0 +1,28 @@
+﻿using ElProyecteGrande.Models.Recipes;
+using ElProyecteGrande.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElProyecteGrande.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        private readonly ElProyecteGrandeContext _context;
+
+        public RecipeController(ElProyecteGrandeContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Recipe>>> GetAllRecipe()
+        {
+            var recipes = await _context.Recipes.ToListAsync();
+            return Ok(recipes);
+        }
+    }
+}
