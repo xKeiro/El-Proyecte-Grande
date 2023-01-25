50ad1029b4ccc7dbc745aaa491518f7910a38e0e
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-01-25 15:03:14 +0100

    Add controller for DishType and MealTime

---
+++ ElProyecteGrande/Controllers/DishTypeController.cs
@@ -0,0 +1,17 @@
+﻿using ElProyecteGrande.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElProyecteGrande.Controllers
+{
+    [ApiController]
+    [Route("[controller]/api/")]
+    public class DishTypeController : ControllerBase
+    {
+        private readonly IDishTypeService _dishTypeService;
+
+        public DishTypeController(IDishTypeService dishTypeService)
+        {
+            _dishTypeService = dishTypeService;
+        }
+    }
+}
---
+++ ElProyecteGrande/Controllers/MealTimeController.cs
@@ -0,0 +1,18 @@
+﻿using ElProyecteGrande.Models.Categories;
+using ElProyecteGrande.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElProyecteGrande.Controllers
+{
+    [ApiController]
+    [Route("[controller]/api/")]
+    public class MealTimeController : ControllerBase
+    {
+        private readonly IMealTimeService _mealTimeService;
+
+        public MealTimeController(IMealTimeService mealTimeService)
+        {
+            _mealTimeService = mealTimeService;
+        }
+    }
+}
