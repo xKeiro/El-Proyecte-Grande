8a393c358c0e8a80392925efd3c54e0fc472660c
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-01-26 18:17:20 +0100

    Add Id property to UserPublic

--- ElProyecteGrande/Models/Dto/Users/UserPublic.cs
+++ ElProyecteGrande/Models/Dto/Users/UserPublic.cs
@@ -2,6 +2,7 @@
 {
     public class UserPublic
     {
+        public required int Id { get; set; }
         public required string Username { get; set; }
 
         public string? FirstName { get; set; }
