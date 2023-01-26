a17251930e1583785af8ccc797b3a93b19a25c38
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-01-26 18:53:29 +0100

    Add endpoint to create a new user

--- ElProyecteGrande/Controllers/UserController.cs
+++ ElProyecteGrande/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using ElProyecteGrande.Interfaces.Services;
 using ElProyecteGrande.Models;
 using ElProyecteGrande.Models.Categories;
+using ElProyecteGrande.Models.Dto.Categories;
 using ElProyecteGrande.Models.Dto.Users;
 using ElProyecteGrande.Models.Users;
 using ElProyecteGrande.Services.Categories;
@@ -32,6 +33,31 @@ namespace ElProyecteGrande.Controllers
                 return StatusCode(StatusCodes.Status200OK, users);
             }
             return StatusCode(StatusCodes.Status404NotFound, _statusMessageService.NoneFound());
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusMessage))]
+        [ProducesResponseType(StatusCodes.Status406NotAcceptable, Type = typeof(StatusMessage))]
+        public async Task<ActionResult<User>> AddNewUser(UserWithoutId newUser)
+        {
+            User user = new User();
+            newUser.MapTo(user);
+
+            if (!await _userService.IsUnique(user))
+            {
+                return StatusCode(StatusCodes.Status406NotAcceptable, _statusMessageService.NotUnique());
+            }
+            try
+            {
+                await _userService.Add(user);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, _statusMessageService.GenericError());
+            }
+
+            return StatusCode(StatusCodes.Status201Created, user);
         }
 
         [HttpGet("{id}")]
