6411986e3a6baaf955a127652066b72b6a9189a8
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-01-26 13:22:12 +0100

    Refactor remove blank lines

--- ElProyecteGrande/Models/Categories/MealTime.cs
+++ ElProyecteGrande/Models/Categories/MealTime.cs
@@ -6,7 +6,6 @@ namespace ElProyecteGrande.Models.Categories;
 /// <summary>
 /// Time of a meal, like - breakfast, lunch, dinner
 /// </summary>
-
 [Index(nameof(Name), IsUnique = true)]
 public class MealTime : BaseCategory
 {
