af18b536764c86884323269b951456e272d3b9bc
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-01-26 18:48:38 +0100

    Add UserWithoutId DTO

---
+++ ElProyecteGrande/Models/Dto/Users/UserWithoutId.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ElProyecteGrande.Models.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElProyecteGrande.Models.Dto.Users
+{
+    public class UserWithoutId
+    {
+        [Required]
+        public required string Username { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        [Required]
+        [RegularExpression("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$", ErrorMessage = "Invalid pattern")]
+        public required string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
+
+        [Required]
+        public required bool IsAdmin { get; set; } = false;
+
+        public void MapTo(User user)
+        {
+            user.Username = Username;
+            user.FirstName = FirstName;
+            user.LastName = LastName;
+            user.EmailAddress = EmailAddress;
+            user.Password = Password;
+            user.IsAdmin = IsAdmin;
+        }
+    }
+}
