49f791329b8fcb3fed7bc1cc7f02d64d4c716179
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-01-26 17:42:11 +0100

    Add UserPublic DTO

---
+++ ElProyecteGrande/Models/Dto/Users/UserPublic.cs
@@ -0,0 +1,15 @@
+﻿namespace ElProyecteGrande.Models.Dto.Users
+{
+    public class UserPublic
+    {
+        public required string Username { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public required string EmailAddress { get; set; }
+
+        public bool IsAdmin { get; set; }
+    }
+}
