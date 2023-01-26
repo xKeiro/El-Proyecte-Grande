efc4b4134987600f24b730274a104eb95d723ccc
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-01-26 16:40:10 +0100

    Update UserService to implement IBasicCrudService interface

--- ElProyecteGrande/Services/Users/UserService.cs
+++ ElProyecteGrande/Services/Users/UserService.cs
@@ -1,6 +1,46 @@
-﻿namespace ElProyecteGrande.Services.Users
+﻿using ElProyecteGrande.Interfaces.Services;
+using ElProyecteGrande.Models.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElProyecteGrande.Services.Users
 {
-    public class UserService
+    public class UserService : IBasicCrudService<User>
     {
+        private readonly ElProyecteGrandeContext _context;
+
+        public UserService(ElProyecteGrandeContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<User>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User?> Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Delete(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> IsUnique(User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
