19aac172e41323c7e1573d271f8c7c6c4dd2630b
xKeiro <nkevin@outlook.hu>
2023-01-26 22:21:10 +0100

    Add DietWithoutCategorization DTO

---
+++ ElProyecteGrande/Models/Dto/Categories/DietWithoutCategorization.cs
@@ -0,0 +1,36 @@
+﻿using ElProyecteGrande.Models.Categories;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElProyecteGrande.Models.DTOs.Categories;
+
+/// <summary>
+/// Different type of diets, like - vegetarian, gluten free, paleo etc.
+/// </summary>
+
+[Index(nameof(Name), IsUnique = true)]
+public class DietWithoutCategorization: BaseCategory
+{
+    public DietWithoutCategorization()
+    {
+        
+    }
+
+    public DietWithoutCategorization( Diet diet)
+    {
+        Name = diet.Name;
+        Id = diet.Id;
+    }
+
+    public void MapTo(Diet diet)
+    {
+        diet.Name = Name;
+        diet.Id = Id;
+    }
+
+    public void MapFrom(Diet diet)
+    {
+        Name = diet.Name;
+        Id = diet.Id;
+    }
+}
