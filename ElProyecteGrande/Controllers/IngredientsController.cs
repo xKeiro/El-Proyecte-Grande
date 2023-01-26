81f7c769cbfb63c6e9eab4e3ead0d14ca80b83df
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-01-26 15:07:29 +0100

    Refactor rename constructor first parameter name from context to service

--- ElProyecteGrande/Controllers/IngredientsController.cs
+++ ElProyecteGrande/Controllers/IngredientsController.cs
@@ -13,9 +13,9 @@ public class IngredientsController : ControllerBase
     private readonly IBasicCrudService<Ingredient> _service;
     private readonly IStatusMessageService<Ingredient> _statusMessage;
 
-    public IngredientsController(IBasicCrudService<Ingredient> context, IStatusMessageService<Ingredient> statusMessage)
+    public IngredientsController(IBasicCrudService<Ingredient> service, IStatusMessageService<Ingredient> statusMessage)
     {
-        _service = context;
+        _service = service;
         _statusMessage = statusMessage;
     }
     [HttpGet]
