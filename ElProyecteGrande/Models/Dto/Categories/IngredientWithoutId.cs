5e7ef6c3654f5f83e4d78733fb048fd551995e59
xKeiro <nkevin@outlook.hu>
2023-01-26 07:43:27 +0100

    Add DTO for Ingredtion without Id

---
+++ ElProyecteGrande/Models/Dto/Categories/IngredientWithoutId.cs
@@ -0,0 +1,26 @@
+﻿using ElProyecteGrande.Models.Categories;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElProyecteGrande.Models.DTOs.Categories;
+
+/// <summary>
+/// Different type of ingredients, like - Hungarian, Italian, French, etc.
+/// </summary>
+
+[Index(nameof(Name), IsUnique = true)]
+public class IngredientWithoutId
+{
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(60, MinimumLength = 2)]
+    public string Name { get; set; }
+
+    [Required]
+    public string UnitOfMeasure { get; set; }
+
+    public void MapTo(Ingredient ingredient)
+    {
+        ingredient.Name = Name;
+        ingredient.UnitOfMeasure = UnitOfMeasure;
+    }
+}
