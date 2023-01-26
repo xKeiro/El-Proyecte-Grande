147996c632c9bb283510492d858f79b11b3f3d43
Lilla Meszaros <ameszaroslilla@gmail.com>
2023-01-26 11:57:18 +0100

    Add Json Serialization

--- ElProyecteGrande/Program.cs
+++ ElProyecteGrande/Program.cs
@@ -8,12 +8,15 @@ using ElProyecteGrande.Models;
 using ElProyecteGrande.Interfaces.Services.Categories;
 using ElProyecteGrande.Services.Categories;
 using ElProyecteGrande.Data;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles
+);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<ElProyecteGrandeContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ElProyecteGrandeContext")));
