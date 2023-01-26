44a8ed4519042f146f385cdd36b6665774de2fa1
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-01-26 14:57:41 +0100

    Add endpoint to delete a meal time by Id

--- ElProyecteGrande/Controllers/MealTimeController.cs
+++ ElProyecteGrande/Controllers/MealTimeController.cs
@@ -86,5 +86,27 @@ namespace ElProyecteGrande.Controllers
             }
             return StatusCode(StatusCodes.Status200OK, mealTime);
         }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusMessage))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(StatusMessage))]
+        public async Task<ActionResult<StatusMessage>> DeleteMealTimeById(int id)
+        {
+            MealTime? mealTime = await _mealTimeService.Find(id);
+            if (mealTime == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, _statusMessageService.NotFound(id));
+            }
+            try
+            {
+                await _mealTimeService.Delete(mealTime);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, _statusMessageService.GenericError());
+            }
+            return StatusCode(StatusCodes.Status200OK, _statusMessageService.Deleted(id));
+        }
     }
 }
