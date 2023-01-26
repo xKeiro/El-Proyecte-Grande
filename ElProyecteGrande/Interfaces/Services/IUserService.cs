51b08b4306353640285654a298b0747eab7d5f47
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-01-26 17:42:48 +0100

    Add IUserService interface with GetAll method

---
+++ ElProyecteGrande/Interfaces/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using ElProyecteGrande.Models.Dto.Users;
+using ElProyecteGrande.Models.Users;
+
+namespace ElProyecteGrande.Interfaces.Services
+{
+    public interface IUserService
+    {
+        Task<List<UserPublic>> GetAll();
+    }
+}
