f282c7ce3da1466f80ead55e31335c4ca5bc380e
xKeiro <nkevin@outlook.hu>
2023-01-26 07:26:16 +0100

    Add StatusMessageService service

---
+++ ElProyecteGrande/Services/StatusMessageService.cs
@@ -0,0 +1,36 @@
+﻿
+//import controller StatusCode
+using ElProyecteGrande.Interfaces.Services;
+using ElProyecteGrande.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElProyecteGrande.Services
+{
+    public class StatusMessageService<T> : IStatusMessageService<T> where T : class
+    {
+        public StatusMessage NoneFound() => new() 
+        { 
+            Message = $"No {typeof(T).Name}s found!" 
+        };
+        public StatusMessage AlreadyExists() => new()
+        {
+            Message = $"{typeof(T).Name} already exists!"
+        };
+        public StatusMessage NotFound(int id) => new()
+        {
+            Message = $"{typeof(T).Name} with id:'{id}' doesn't exist!"
+        };
+        public StatusMessage Deleted(int id) => new()
+        {
+            Message = $"{typeof(T).Name} with id:'{id}' was deleted!"
+        };
+        public StatusMessage NotUnique() => new()
+        {
+            Message = $"{typeof(T).Name} has a property that is not unique, but should be!"
+        };
+        public StatusMessage GenericError() => new()
+        {
+            Message = "There was a problem!"
+        };
+    }
+}
