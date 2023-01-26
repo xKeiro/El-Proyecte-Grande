1f5a25f1e37eb4b40b088081a0ba2a0cba12dc4e
Lilla Meszaros <ameszaroslilla@gmail.com>
2023-01-26 12:25:41 +0100

    Add summary to Diet model

--- ElProyecteGrande/Models/Categories/Diet.cs
+++ ElProyecteGrande/Models/Categories/Diet.cs
@@ -3,6 +3,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ElProyecteGrande.Models.Categories;
 
+/// <summary>
+/// Different type of diets, like - vegetarian, gluten free, paleo etc.
+/// </summary>
+/// 
 [Index(nameof(Name), IsUnique = true)]
 public class Diet : BaseCategory
 {
