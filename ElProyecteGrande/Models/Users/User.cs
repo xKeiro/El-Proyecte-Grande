a8de0ba95cc0611165b31dad7bdb0545199d2be6
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-01-26 18:48:01 +0100

    Remove required keyword from properties

--- ElProyecteGrande/Models/Users/User.cs
+++ ElProyecteGrande/Models/Users/User.cs
@@ -1,11 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace ElProyecteGrande.Models.Users
 {
     public class User : BaseModel
     {
         [Required]
-        public required string Username { get; set; }
+        public string Username { get; set; }
 
         public string? FirstName { get; set; }
 
@@ -13,13 +14,13 @@ namespace ElProyecteGrande.Models.Users
 
         [Required]
         [RegularExpression("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$", ErrorMessage = "Invalid pattern")]
-        public required string EmailAddress { get; set; }
+        public string EmailAddress { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        public required string Password { get; set; }
+        public string Password { get; set; }
 
         [Required]
-        public required bool IsAdmin { get; set; } = false;
+        public bool IsAdmin { get; set; } = false;
     }
 }
