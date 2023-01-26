d52ddc995891d02c265837c02b7443750fc91248
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-01-26 14:00:53 +0100

    Update DishTypeController with the new structure

--- ElProyecteGrande/Controllers/DishTypeController.cs
+++ ElProyecteGrande/Controllers/DishTypeController.cs
@@ -1,54 +1,90 @@
-﻿using ElProyecteGrande.Interfaces.Services.Categories;
+﻿using ElProyecteGrande.Interfaces.Services;
+using ElProyecteGrande.Models;
 using ElProyecteGrande.Models.Categories;
+using ElProyecteGrande.Models.Dto.Categories;
+using ElProyecteGrande.Models.DTOs.Categories;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace ElProyecteGrande.Controllers
 {
     [ApiController]
-    [Route("[controller]/api/")]
+    [Route("api/[controller]")]
     public class DishTypeController : ControllerBase
     {
-        private readonly IDishTypeService _dishTypeService;
+        private readonly IBasicCrudService<DishType> _dishTypeService;
+        private readonly IStatusMessageService<DishType> _statusMessageService;
 
-        public DishTypeController(IDishTypeService dishTypeService)
+        public DishTypeController(IBasicCrudService<DishType> dishTypeService, IStatusMessageService<DishType> statusMessageService)
         {
             _dishTypeService = dishTypeService;
+            _statusMessageService = statusMessageService;
         }
 
-        [Route("[action]")]
         [HttpGet]
-        public async Task<IEnumerable<DishType>> GetDishTypes()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(StatusMessage))]
+        public async Task<ActionResult<IEnumerable<DishType>>> GetDishTypes()
         {
-            IEnumerable<DishType> mealTimes = await _dishTypeService.GetAllDishType();
-            return mealTimes;
+            List<DishType>? dishTypes = await _dishTypeService.GetAll();
+            if (dishTypes is not null)
+            {
+                return StatusCode(StatusCodes.Status200OK, dishTypes);
+            }
+            return StatusCode(StatusCodes.Status404NotFound, _statusMessageService.NoneFound());
         }
 
-        [Route("[action]")]
         [HttpPost]
-        public async Task<IResult> AddNewDishType([FromBody] string dishTypeName)
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusMessage))]
+        [ProducesResponseType(StatusCodes.Status406NotAcceptable, Type = typeof(StatusMessage))]
+        public async Task<ActionResult<Cuisine>> AddNewDishType(DishTypeWithoutId newDishType)
         {
+            DishType dishType = new DishType();
+            newDishType.MapTo(dishType);
+
+            if (!await _dishTypeService.IsUnique(dishType))
+            {
+                return StatusCode(StatusCodes.Status406NotAcceptable, _statusMessageService.NotUnique());
+            }
             try
             {
-                await _dishTypeService.AddDishType(new DishType { Name = dishTypeName });
-                return Results.Ok($"{dishTypeName} has been added to the Dish Types.");
+                await _dishTypeService.Add(dishType);
             }
-            catch (DbUpdateException)
+            catch
             {
-                return Results.Conflict($"We already have this Dish Type: {dishTypeName}!");
+                return StatusCode(StatusCodes.Status400BadRequest, _statusMessageService.GenericError());
             }
+
+            return StatusCode(StatusCodes.Status201Created, dishType);
         }
 
-        [Route("[action]")]
-        [HttpPatch]
-        public async Task<IResult> UpdateDishType(string dishTypeName, [FromBody] string newDishTypeName)
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusMessage))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(StatusMessage))]
+        [ProducesResponseType(StatusCodes.Status406NotAcceptable, Type = typeof(StatusMessage))]
+        public async Task<ActionResult<Cuisine>> UpdateDishTypeById(int id, DishTypeWithoutId newDishType)
         {
-            DishType mealTimeToChange = await _dishTypeService.GetDishTypeByName(dishTypeName);
-            if (mealTimeToChange is null) return Results.NotFound($"We don't have this Dish Type: {dishTypeName}!");
+            DishType? dishType = await _dishTypeService.Find(id);
+            if (dishType == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, _statusMessageService.NotFound(id));
+            }
 
-            mealTimeToChange.Name = newDishTypeName;
-            _dishTypeService.UpdateDishType(mealTimeToChange);
-            return Results.Ok($"{dishTypeName} has been successfully updated to {newDishTypeName}.");
+            newDishType.MapTo(dishType);
+            if (!await _dishTypeService.IsUnique(dishType))
+            {
+                return StatusCode(StatusCodes.Status406NotAcceptable, _statusMessageService.NotUnique());
+            }
+            try
+            {
+                await _dishTypeService.Update(dishType);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, _statusMessageService.GenericError());
+            }
+            return StatusCode(StatusCodes.Status200OK, dishType);
         }
     }
 }
