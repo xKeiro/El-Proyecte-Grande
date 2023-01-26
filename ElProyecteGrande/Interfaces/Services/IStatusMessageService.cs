7b343a6a72f81c60493124d269eb367e8eab9347
xKeiro <nkevin@outlook.hu>
2023-01-26 05:53:28 +0100

    Add IStatusMessageService interface

---
+++ ElProyecteGrande/Interfaces/Services/IStatusMessageService.cs
@@ -0,0 +1,12 @@
+﻿namespace ElProyecteGrande.Interfaces.Services
+{
+    public interface IStatusMessageService<T> where T: class
+    {
+        string AlreadyExists();
+        string Deleted(int id);
+        string NoneFound();
+        string NotFound(int id);
+        string NotUnique();
+        string GenericError();
+    }
+}
