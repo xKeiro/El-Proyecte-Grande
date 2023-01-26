2807913f7c5516e1db93c2191c12ca71e318afb7
xKeiro <nkevin@outlook.hu>
2023-01-26 07:32:28 +0100

    Add DTO for Cuisine without Id

---
+++ ElProyecteGrande/Models/Dto/Categories/CuisineWithoutId.cs
@@ -0,0 +1,22 @@
+﻿using ElProyecteGrande.Models.Categories;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElProyecteGrande.Models.DTOs.Categories;
+
+/// <summary>
+/// Different type of cuisines, like - Hungarian, Italian, French, etc.
+/// </summary>
+
+[Index(nameof(Name), IsUnique = true)]
+public class CuisineWithoutId
+{
+    [Required]
+    [StringLength(20, MinimumLength = 2)]
+    public required string Name { get; set; }
+
+    public void MapTo(Cuisine cuisine)
+    {
+        cuisine.Name = Name;
+    }
+}
