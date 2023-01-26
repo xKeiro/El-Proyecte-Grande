64e286e503fee5e542ff096efdc23fe0200c762d
Lilla Meszaros <ameszaroslilla@gmail.com>
2023-01-26 11:40:03 +0100

    Add DietsController controller

---
+++ ElProyecteGrande/Controllers/DietsController.cs
@@ -0,0 +1,50 @@
+﻿using ElProyecteGrande.Interfaces.Services;
+using ElProyecteGrande.Models;
+using ElProyecteGrande.Models.Categories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElProyecteGrande.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DietsController : ControllerBase
+    {
+        private readonly IBasicCrudService<Diet> _service;
+        private readonly IStatusMessageService<Diet> _statusMessage;
+
+        public DietsController(IBasicCrudService<Diet> service, IStatusMessageService<Diet> statusMessage)
+        {
+            _service = service;
+            _statusMessage = statusMessage;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(StatusMessage))]
+        public async Task<ActionResult<IEnumerable<Diet>>> GetAllDiets()
+        {
+            var diets = await _service.GetAll();
+            if (diets != null)
+            {
+                return StatusCode(StatusCodes.Status200OK, diets);
+            }
+
+            return StatusCode(StatusCodes.Status404NotFound, _statusMessage.NoneFound());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(StatusMessage))]
+        public async Task<ActionResult<IEnumerable<Diet>>> GetDietById(int id)
+        {
+            var dietById = await _service.Find(id);
+            if (dietById != null)
+            {
+                return StatusCode(StatusCodes.Status200OK, dietById);
+            }
+            return StatusCode(StatusCodes.Status404NotFound, _statusMessage.NotFound(id));
+        }
+
+    }
+}
--- ElProyecteGrande/Program.cs
+++ ElProyecteGrande/Program.cs
@@ -19,8 +19,10 @@ builder.Services.AddDbContext<ElProyecteGrandeContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ElProyecteGrandeContext")));
 builder.Services.AddScoped<IBasicCrudService<Cuisine>, CuisineService>();
 builder.Services.AddScoped<IBasicCrudService<Ingredient>, IngredientService>();
+builder.Services.AddScoped<IBasicCrudService<Diet>, DietService>();
 builder.Services.AddScoped<IStatusMessageService<Cuisine>, StatusMessageService<Cuisine>>();
 builder.Services.AddScoped<IStatusMessageService<Ingredient>, StatusMessageService<Ingredient>>();
+builder.Services.AddScoped<IStatusMessageService<Diet>, StatusMessageService<Diet>>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
