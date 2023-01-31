8a0c17fd38cfcabab6d56cd24dc6726ed293636e
xKeiro <nkevin@outlook.hu>
2023-01-31 18:45:11 +0100

    Add DTOs for RecipeStatus

---
+++ ElProyecteGrande/Dtos/Recipes/RecipeReview/RecipeReviewFull.cs
@@ -0,0 +1,6 @@
+﻿namespace ElProyecteGrande.Dtos.Recipes.RecipeReview;
+
+public class RecipeReviewFull: RecipeReviewWithoutId
+{
+    public int Id { get; set; }
+}
---
+++ ElProyecteGrande/Dtos/Recipes/RecipeReview/RecipeReviewWithoutId.cs
@@ -0,0 +1,20 @@
+﻿using ElProyecteGrande.Dtos.Recipes.Recipe;
+using ElProyecteGrande.Models.Users;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElProyecteGrande.Dtos.Recipes.RecipeReview;
+
+public class RecipeReviewWithoutId
+{
+    [Required]
+    public required User User { get; set; }
+
+    [Required]
+    public required RecipeFull Recipe { get; set; }
+
+    [Required]
+    [Range(1, 5)]
+    public required int Rate { get; set; }
+
+    public string? Review { get; set; }
+}
