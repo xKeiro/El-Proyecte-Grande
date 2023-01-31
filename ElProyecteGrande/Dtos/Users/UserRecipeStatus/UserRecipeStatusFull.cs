cdafbfa36c28666395e0b097de8c4ac30e558b13
xKeiro <nkevin@outlook.hu>
2023-01-31 18:52:07 +0100

    Add DTOs for UserRecipeStatus

---
+++ ElProyecteGrande/Dtos/Users/UserRecipeStatus/UserRecipeStatusFull.cs
@@ -0,0 +1,6 @@
+﻿namespace ElProyecteGrande.Dtos.Users.UserRecipeStatus;
+
+public class UserRecipeStatusFull : UserRecipeStatusWithoutId
+{
+    public required int Id { get; set; }
+}
---
+++ ElProyecteGrande/Dtos/Users/UserRecipeStatus/UserRecipeStatusWithoutId.cs
@@ -0,0 +1,10 @@
+﻿using ElProyecteGrande.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElProyecteGrande.Dtos.Users.UserRecipeStatus;
+
+public class UserRecipeStatusWithoutId
+{
+    [Required]
+    public required RecipeStatus Name { get; set; }
+}
