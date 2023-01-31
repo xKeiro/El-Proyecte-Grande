648e72a33ae9e6ad90860b21a9822e368e65d575
xKeiro <nkevin@outlook.hu>
2023-01-31 18:11:29 +0100

    Add DTOs for Recipe

---
+++ ElProyecteGrande/Dtos/Recipes/Recipe/RecipeFull.cs
@@ -0,0 +1,6 @@
+﻿namespace ElProyecteGrande.Dtos.Recipes.Recipe;
+
+public class RecipeFull: RecipeWithoutId
+{
+    public int Id { get; set; }
+}
---
+++ ElProyecteGrande/Dtos/Recipes/Recipe/RecipeWithoutId.cs
@@ -0,0 +1,30 @@
+﻿using ElProyecteGrande.Dtos.Categories.Cuisine;
+using ElProyecteGrande.Dtos.Recipes.RecipeIngredient;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElProyecteGrande.Dtos.Recipes.Recipe;
+
+public class RecipeWithoutId
+{
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(120, MinimumLength = 2)]
+    public required string Name { get; set; }
+    
+    [Required]
+    public required string Description { get; set; }
+
+    [Required]
+    [MinLength(1)]
+    public required ICollection<RecipeIngredientFull> RecipeIngredients { get; set; }
+
+    [Required]
+    public CuisineFull Cuisine { get; set; }
+    [Required]
+    [MinLength(1)]
+    public ICollection<MealTimeFull> MealTimes { get; set; }
+    [Required]
+    [MinLength(1)]
+    public ICollection<DietFull> Diets { get; set; }
+    [Required]
+    public DishTypeFull DishType { get; set; }
+}
