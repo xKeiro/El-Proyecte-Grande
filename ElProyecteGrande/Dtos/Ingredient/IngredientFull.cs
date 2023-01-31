b4845c5d694af9610610c6e48235c666f24a96dc
xKeiro <nkevin@outlook.hu>
2023-01-31 18:10:59 +0100

    Add DTOs for Ingredient

---
+++ ElProyecteGrande/Dtos/Ingredient/IngredientFull.cs
@@ -0,0 +1,6 @@
+﻿namespace ElProyecteGrande.Dtos.Ingredient;
+
+public class IngredientFull: IngredientWithoutId
+{
+    public int Id { get; set; }
+}
---
+++ ElProyecteGrande/Dtos/Ingredient/IngredientWithoutId.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElProyecteGrande.Dtos.Ingredient;
+
+/// <summary>
+/// Different type of ingredients, like - onion, penne, tomato etc.
+/// </summary>
+
+public class IngredientWithoutId
+{
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(60, MinimumLength = 2)]
+    public string Name { get; set; }
+
+    [Required]
+    public string UnitOfMeasure { get; set; }
+
+}
