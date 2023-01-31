5b1870e1eda627699279efe7d80fe885bb7e5aed
xKeiro <nkevin@outlook.hu>
2023-01-31 18:58:05 +0100

    Update IngredientsController to use new DTOs and automapper

--- ElProyecteGrande/Controllers/IngredientsController.cs
+++ ElProyecteGrande/Controllers/IngredientsController.cs
@@ -1,7 +1,7 @@
-﻿using ElProyecteGrande.Interfaces.Services;
+﻿using AutoMapper;
+using ElProyecteGrande.Dtos.Ingredient;
+using ElProyecteGrande.Interfaces.Services;
 using ElProyecteGrande.Models;
-using ElProyecteGrande.Models.Categories;
-using ElProyecteGrande.Models.DTOs.Categories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ElProyecteGrande.Controllers;
@@ -12,21 +12,26 @@ public class IngredientsController : ControllerBase
 {
     private readonly IBasicCrudService<Ingredient> _service;
     private readonly IStatusMessageService<Ingredient> _statusMessage;
+    private readonly IMapper _mapper;
 
-    public IngredientsController(IBasicCrudService<Ingredient> ingredientService, IStatusMessageService<Ingredient> statusMessage)
+    public IngredientsController(IBasicCrudService<Ingredient> ingredientService,
+        IStatusMessageService<Ingredient> statusMessage,
+        IMapper mapper)
     {
         _service = ingredientService;
         _statusMessage = statusMessage;
+        _mapper = mapper;
     }
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(StatusMessage))]
-    public async Task<ActionResult<IEnumerable<Ingredient>>> GetAllIngredients()
+    public async Task<ActionResult<IEnumerable<IngredientFull>>> GetAllIngredients()
     {
         var ingredients = await _service.GetAll();
         if (ingredients != null)
         {
-            return StatusCode(StatusCodes.Status200OK, ingredients);
+            var ingredientsFull = _mapper.Map<IEnumerable<Ingredient>, IEnumerable<IngredientFull>>(ingredients);
+            return StatusCode(StatusCodes.Status200OK, ingredientsFull);
         }
         return StatusCode(StatusCodes.Status404NotFound, _statusMessage.NoneFound());
     }
@@ -34,14 +39,13 @@ public class IngredientsController : ControllerBase
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusMessage))]
-    [ProducesResponseType(StatusCodes.Status406NotAcceptable, Type = typeof(StatusMessage))]
-    public async Task<ActionResult<Ingredient>> AddIngredient(IngredientWithoutId ingredientWithoutId)
+    [ProducesResponseType(StatusCodes.Status409Conflict, Type = typeof(StatusMessage))]
+    public async Task<ActionResult<IngredientFull>> AddIngredient(IngredientWithoutId ingredientWithoutId)
     {
-        var ingredient = new Ingredient();
-        ingredientWithoutId.MapTo(ingredient);
+        var ingredient = _mapper.Map<IngredientWithoutId, Ingredient>(ingredientWithoutId);
         if (!await _service.IsUnique(ingredient))
         {
-            return StatusCode(StatusCodes.Status406NotAcceptable, _statusMessage.NotUnique());
+            return StatusCode(StatusCodes.Status409Conflict, _statusMessage.NotUnique());
         }
         try
         {
@@ -51,19 +55,20 @@ public class IngredientsController : ControllerBase
         {
             return StatusCode(StatusCodes.Status400BadRequest, _statusMessage.GenericError());
         }
-
-        return StatusCode(StatusCodes.Status201Created, ingredient);
+        var ingredientFull = _mapper.Map<Ingredient, IngredientFull>(ingredient);
+        return StatusCode(StatusCodes.Status201Created, ingredientFull);
     }
 
     [HttpGet("{id}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(StatusMessage))]
-    public async Task<ActionResult<Ingredient>> GetIngredientById(int id)
+    public async Task<ActionResult<IngredientFull>> GetIngredientById(int id)
     {
         var ingredient = await _service.Find(id);
         if (ingredient != null)
         {
-            return StatusCode(StatusCodes.Status200OK, ingredient);
+            var ingredientFull = _mapper.Map<Ingredient, IngredientFull>(ingredient);
+            return StatusCode(StatusCodes.Status200OK, ingredientFull);
         }
         return StatusCode(StatusCodes.Status404NotFound, _statusMessage.NotFound(id));
     }
@@ -72,18 +77,18 @@ public class IngredientsController : ControllerBase
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusMessage))]
     [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(StatusMessage))]
-    [ProducesResponseType(StatusCodes.Status406NotAcceptable, Type = typeof(StatusMessage))]
-    public async Task<ActionResult<Ingredient>> UpdateIngredientById(int id, IngredientWithoutId ingredientWithoutId)
+    [ProducesResponseType(StatusCodes.Status409Conflict, Type = typeof(StatusMessage))]
+    public async Task<ActionResult<IngredientFull>> UpdateIngredientById(int id, IngredientWithoutId ingredientWithoutId)
     {
         var ingredient = await _service.Find(id);
         if (ingredient == null)
         {
             return StatusCode(StatusCodes.Status404NotFound, _statusMessage.NotFound(id));
         }
-        ingredientWithoutId.MapTo(ingredient);
+        ingredient = _mapper.Map<IngredientWithoutId, Ingredient>(ingredientWithoutId);
         if (!await _service.IsUnique(ingredient))
         {
-            return StatusCode(StatusCodes.Status406NotAcceptable, _statusMessage.NotUnique());
+            return StatusCode(StatusCodes.Status409Conflict, _statusMessage.NotUnique());
         }
         try
         {
@@ -93,7 +98,8 @@ public class IngredientsController : ControllerBase
         {
             return StatusCode(StatusCodes.Status400BadRequest, _statusMessage.GenericError());
         }
-        return StatusCode(StatusCodes.Status200OK, ingredient);
+        var ingredientFull = _mapper.Map<Ingredient, IngredientFull>(ingredient);
+        return StatusCode(StatusCodes.Status200OK, ingredientFull);
     }
 
     [HttpDelete("{id}")]
