12a6bef7718f8e0ef085638d5a2dc2d927a6bc94
xKeiro <nkevin@outlook.hu>
2023-01-31 18:12:11 +0100

    Add DTOs for RecipeIngredient

---
+++ ElProyecteGrande/Dtos/Recipes/RecipeIngredient/RecipeIngredientFull.cs
@@ -0,0 +1,6 @@
+﻿namespace ElProyecteGrande.Dtos.Recipes.RecipeIngredient;
+
+public class RecipeIngredientFull: RecipeIngredientWithoutId
+{
+    public int Id { get; set; }
+}
---
+++ ElProyecteGrande/Dtos/Recipes/RecipeIngredient/RecipeIngredientWithoutId.cs
@@ -0,0 +1,14 @@
+﻿using ElProyecteGrande.Dtos.Ingredient;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElProyecteGrande.Dtos.Recipes.RecipeIngredient;
+
+public class RecipeIngredientWithoutId
+{
+    [Required]
+    public required IngredientFull Ingredient { get; set; }
+    [Required]
+    [Precision(6, 2)]
+    public required decimal Amount { get; set; }
+}
