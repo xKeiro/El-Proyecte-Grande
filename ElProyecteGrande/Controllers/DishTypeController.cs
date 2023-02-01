3eee9d60214fb7b41566058b772f1178359ede53
xKeiro <nkevin@outlook.hu>
2023-02-01 16:53:14 +0100

    Update DishType Service and Controller to do mapping inside the service and remove Delete route

--- ElProyecteGrande/Controllers/DishTypeController.cs
+++ ElProyecteGrande/Controllers/DishTypeController.cs
@@ -1,132 +1,112 @@
-﻿using AutoMapper;
-using ElProyecteGrande.Dtos.Categories.Cuisine;
-using ElProyecteGrande.Dtos.Categories.DishType;
+﻿using ElProyecteGrande.Dtos.Categories.DishType;
 using ElProyecteGrande.Interfaces.Services;
 using ElProyecteGrande.Models;
 using ElProyecteGrande.Models.Categories;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ElProyecteGrande.Controllers
+namespace ElProyecteGrande.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DishTypesController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class DishTypeController : ControllerBase
+    private readonly IBasicCrudService<DishTypePublic, DishTypeWithoutId> _service;
+    private readonly IStatusMessageService<DishType> _statusMessage;
+
+    public DishTypesController(IBasicCrudService<DishTypePublic, DishTypeWithoutId> dishTypeService,
+        IStatusMessageService<DishType> statusMessage)
     {
-        private readonly IBasicCrudService<DishType> _dishTypeService;
-        private readonly IStatusMessageService<DishType> _statusMessageService;
-        private readonly IMapper _mapper;
-
-        public DishTypeController(IBasicCrudService<DishType> dishTypeService,
-            IStatusMessageService<DishType> statusMessageService,
-            IMapper mapper)
+        _service = dishTypeService;
+        _statusMessage = statusMessage;
+    }
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusMessage))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(StatusMessage))]
+    public async Task<ActionResult<IEnumerable<DishTypePublic>>> GetAllDishTypes()
+    {
+        try
         {
-            _dishTypeService = dishTypeService;
-            _statusMessageService = statusMessageService;
-            _mapper = mapper;
-        }
-
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(StatusMessage))]
-        public async Task<ActionResult<IEnumerable<DishTypePublic>>> GetDishTypes()
-        {
-            List<DishType>? dishTypes = await _dishTypeService.GetAll();
-            if (dishTypes is not null)
+            var dishTypesPublic = await _service.GetAll();
+            if (dishTypesPublic != null)
             {
-                var dishTypesPublic = _mapper.Map<IEnumerable<DishType>, IEnumerable<DishTypePublic>>(dishTypes);
                 return StatusCode(StatusCodes.Status200OK, dishTypesPublic);
             }
-            return StatusCode(StatusCodes.Status404NotFound, _statusMessageService.NoneFound());
+            return StatusCode(StatusCodes.Status404NotFound, _statusMessage.NoneFound());
         }
-
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusMessage))]
-        [ProducesResponseType(StatusCodes.Status409Conflict, Type = typeof(StatusMessage))]
-        public async Task<ActionResult<DishTypePublic>> AddNewDishType(DishTypeWithoutId dishTypeWithoutId)
+        catch
         {
-            var dishType = _mapper.Map<DishTypeWithoutId, DishType>(dishTypeWithoutId);
+            return StatusCode(StatusCodes.Status400BadRequest, _statusMessage.GenericError());
+        }
+    }
 
-            if (!await _dishTypeService.IsUnique(dishType))
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusMessage))]
+    [ProducesResponseType(StatusCodes.Status409Conflict, Type = typeof(StatusMessage))]
+    public async Task<ActionResult<DishTypePublic>> AddDishType(DishTypeWithoutId dishTypeWithoutId)
+    {
+
+        try
+        {
+            if (!await _service.IsUnique(dishTypeWithoutId))
             {
-                return StatusCode(StatusCodes.Status409Conflict, _statusMessageService.NotUnique());
+                return StatusCode(StatusCodes.Status409Conflict, _statusMessage.NotUnique());
             }
-            try
-            {
-                await _dishTypeService.Add(dishType);
-            }
-            catch
-            {
-                return StatusCode(StatusCodes.Status400BadRequest, _statusMessageService.GenericError());
-            }
-            var dishTypePublic = _mapper.Map<DishType, DishTypePublic>(dishType);
+            var dishTypePublic = await _service.Add(dishTypeWithoutId);
             return StatusCode(StatusCodes.Status201Created, dishTypePublic);
         }
-
-        [HttpGet("{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(StatusMessage))]
-        public async Task<ActionResult<DishTypePublic>> GetDishTypeById(int id)
+        catch
         {
-            DishType? dishType = await _dishTypeService.Find(id);
-            if (dishType is not null)
+            return StatusCode(StatusCodes.Status400BadRequest, _statusMessage.GenericError());
+        }
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusMessage))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(StatusMessage))]
+    public async Task<ActionResult<DishTypePublic>> GetDishTypeById(int id)
+    {
+        try
+        {
+            var dishTypePublic = await _service.Find(id);
+            if (dishTypePublic != null)
             {
-                var dishTypePublic = _mapper.Map<DishType, DishTypePublic>(dishType);
                 return StatusCode(StatusCodes.Status200OK, dishTypePublic);
             }
-            return StatusCode(StatusCodes.Status404NotFound, _statusMessageService.NotFound(id));
+            return StatusCode(StatusCodes.Status404NotFound, _statusMessage.NotFound(id));
         }
-
-        [HttpPut("{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusMessage))]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(StatusMessage))]
-        [ProducesResponseType(StatusCodes.Status409Conflict, Type = typeof(StatusMessage))]
-        public async Task<ActionResult<DishTypePublic>> UpdateDishTypeById(int id, DishTypeWithoutId dishTypeWithoutId)
+        catch
         {
-            DishType? dishType = await _dishTypeService.Find(id);
-            if (dishType == null)
-            {
-                return StatusCode(StatusCodes.Status404NotFound, _statusMessageService.NotFound(id));
-            }
+            return StatusCode(StatusCodes.Status400BadRequest, _statusMessage.GenericError());
+        }
+    }
 
-            dishType = _mapper.Map(dishTypeWithoutId, dishType);
-            if (!await _dishTypeService.IsUnique(dishType))
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusMessage))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(StatusMessage))]
+    [ProducesResponseType(StatusCodes.Status409Conflict, Type = typeof(StatusMessage))]
+    public async Task<ActionResult<DishTypePublic>> UpdateDishTypeById(int id, DishTypeWithoutId dishTypeWithoutId)
+    {
+        try
+        {
+            var dishTypePublicOriginal = await _service.Find(id);
+            if (dishTypePublicOriginal == null)
             {
-                return StatusCode(StatusCodes.Status409Conflict, _statusMessageService.NotUnique());
+                return StatusCode(StatusCodes.Status404NotFound, _statusMessage.NotFound(id));
             }
-            try
+            if (!await _service.IsUnique(dishTypeWithoutId))
             {
-                await _dishTypeService.Update(dishType);
+                return StatusCode(StatusCodes.Status409Conflict, _statusMessage.NotUnique());
             }
-            catch
-            {
-                return StatusCode(StatusCodes.Status400BadRequest, _statusMessageService.GenericError());
-            }
-            var dishTypePublic = _mapper.Map<DishType, DishTypePublic>(dishType);
+            var dishTypePublic = await _service.Update(id, dishTypeWithoutId);
             return StatusCode(StatusCodes.Status200OK, dishTypePublic);
         }
-
-        [HttpDelete("{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusMessage))]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(StatusMessage))]
-        public async Task<ActionResult<StatusMessage>> DeleteDishTypeById(int id)
+        catch
         {
-            DishType? dishType = await _dishTypeService.Find(id);
-            if (dishType == null)
-            {
-                return StatusCode(StatusCodes.Status404NotFound, _statusMessageService.NotFound(id));
-            }
-            try
-            {
-                await _dishTypeService.Delete(dishType);
-            }
-            catch
-            {
-                return StatusCode(StatusCodes.Status400BadRequest, _statusMessageService.GenericError());
-            }
-            return StatusCode(StatusCodes.Status200OK, _statusMessageService.Deleted(id));
+            return StatusCode(StatusCodes.Status400BadRequest, _statusMessage.GenericError());
         }
     }
 }
--- ElProyecteGrande/Services/Categories/DishTypeService.cs
+++ ElProyecteGrande/Services/Categories/DishTypeService.cs
@@ -1,49 +1,64 @@
-﻿using ElProyecteGrande.Interfaces.Services;
-using ElProyecteGrande.Models.Categories;
+﻿using ElProyecteGrande.Models.Categories;
 using Microsoft.EntityFrameworkCore;
+using ElProyecteGrande.Interfaces.Services;
+using ElProyecteGrande.Dtos.Categories.DishType;
+using AutoMapper;
 
 namespace ElProyecteGrande.Services.Categories
 {
-    public class DishTypeService : IBasicCrudService<DishType>
+    public class DishTypeService : IBasicCrudService<DishTypePublic, DishTypeWithoutId>
     {
         private readonly ElProyecteGrandeContext _context;
-
-        public DishTypeService(ElProyecteGrandeContext context)
+        private readonly IMapper _mapper;
+        public DishTypeService(ElProyecteGrandeContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
-        public async Task<List<DishType>> GetAll()
+        public async Task<DishTypePublic> Add(DishTypeWithoutId dishTypeWithoutId)
         {
-            return await _context.DishType.AsNoTracking().ToListAsync();
-        }
-
-        public async Task Add(DishType dishType)
-        {
-            await _context.DishType.AddAsync(dishType);
+            var dishType = _mapper.Map<DishTypeWithoutId, DishType>(dishTypeWithoutId);
+            await _context.DishTypes.AddAsync(dishType);
             await _context.SaveChangesAsync();
+            return _mapper.Map<DishType, DishTypePublic>(dishType);
         }
 
-        public async Task<DishType?> Find(int id)
+        public async Task<List<DishTypePublic>> GetAll()
         {
-            return await _context.DishType.FindAsync(id);
+            var dishTypes = await _context
+                .DishTypes
+                .AsNoTracking()
+                .ToListAsync();
+            return _mapper.Map<List<DishType>, List<DishTypePublic>>(dishTypes);
         }
 
-        public async Task Update(DishType newDishType)
+        public async Task<DishTypePublic?> Find(int id)
         {
-            _context.DishType.Update(newDishType);
+            var dishType = await _context.DishTypes
+                .AsNoTracking()
+                .FirstOrDefaultAsync(c => c.Id == id);
+            if (dishType is null)
+            {
+                return null;
+            }
+            return _mapper.Map<DishType, DishTypePublic>(dishType);
+        }
+
+        public async Task<DishTypePublic> Update(int id, DishTypeWithoutId dishTypeWithoutId)
+        {
+            var dishType = _mapper.Map<DishTypeWithoutId, DishType>(dishTypeWithoutId);
+            dishType.Id = id;
+            _context.Update(dishType);
             await _context.SaveChangesAsync();
+            return _mapper.Map<DishType, DishTypePublic>(dishType);
         }
 
-        public async Task Delete(DishType dishType)
+        public async Task<bool> IsUnique(DishTypeWithoutId dishTypeWithoutId)
         {
-            _context.DishType.Remove(dishType);
-            await _context.SaveChangesAsync();
+            var dishType = _mapper.Map<DishTypeWithoutId, DishType>(dishTypeWithoutId);
+            return !await _context.DishTypes.AnyAsync(c => c.Name == dishType.Name);
         }
 
-        public async Task<bool> IsUnique(DishType dishType)
-        {
-            return !await _context.DishType.AnyAsync(d => d.Name == dishType.Name);
-        }
     }
 }
