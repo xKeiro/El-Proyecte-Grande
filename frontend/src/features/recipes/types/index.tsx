7caae4f414fba61ba20548240600fac8315eb705
xKeiro <nkevin@outlook.hu>
2023-02-08 13:44:15 +0100

    Frontend: Add recipe type and schema

---
+++ frontend/src/features/recipes/types/index.tsx
@@ -0,0 +1,24 @@
+import { ingredientSchema } from '@/features/ingredients';
+import { categorySchema } from '@/features/categories';
+import * as z from 'zod'; 
+
+export const recipeIngredientSchema = z.object({
+    id: z.number().int().positive(),
+    ingredient: ingredientSchema,
+});
+
+export type RecipeIngredient = z.infer<typeof recipeIngredientSchema>;
+
+
+export const recipeSchema = z.object({
+    id: z.number().int().positive(),
+    name: z.string().min(2).max(120),
+    description: z.string(),
+    recipeIngredients: z.array(recipeIngredientSchema),
+    cuisine: categorySchema,
+    mealTimes: z.array(categorySchema),
+    diets: z.array(categorySchema),
+    dishType: categorySchema
+});
+
+export type Recipe = z.infer<typeof recipeSchema>;
