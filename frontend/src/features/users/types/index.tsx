646154a6c90c003197ebb557bcc006d550743ad1
xKeiro <nkevin@outlook.hu>
2023-02-08 13:45:31 +0100

    Frontend: Add user type and schema

---
+++ frontend/src/features/users/types/index.tsx
@@ -0,0 +1,12 @@
+import * as z from 'zod'; 
+
+export const userSchema = z.object({
+    id: z.number().int().positive(),
+    username: z.string().min(2).max(50),
+    emailAddress: z.string().email(),
+    firstName: z.string().min(2).max(100).nullable(),
+    lastName: z.string().min(2).max(100).nullable(),
+    isAdmin: z.boolean()
+    })
+
+export type user = z.infer<typeof userSchema>
