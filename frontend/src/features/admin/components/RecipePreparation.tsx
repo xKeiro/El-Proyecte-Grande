d84cd16a9dee7e08597134f6f60c19213e8cc1ac
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-08 20:45:01 +0100

    Frontend: Add RecipePreparation and RecipeButtons components

---
+++ frontend/src/features/admin/components/RecipePreparation.tsx
@@ -0,0 +1,17 @@
+
+const RecipePreparation = () => {
+    return (
+        <div className="card-body recipe-info bg-base-100">
+            <ol>
+                <li><p>Sift the flour, baking powder, salt and caster sugar into a large bowl. In a separate bowl or jug, lightly whisk together the milk and egg, then whisk in the melted butter.</p></li>
+                <li><p>Pour the milk mixture into the flour mixture and, using a fork, beat until you have a smooth batter. Any lumps will soon disappear with a little mixing. Let the batter stand for a few minutes.</p></li>
+                <li><p>Heat a non-stick frying pan over a medium heat and add a knob of butter. When it's melted, add a ladle of batter (or two if your frying pan is big enough to cook two pancakes at the same time). It will seem very thick but this is how it should be.</p></li>
+                <li><p>Wait until the top of the pancake begins to bubble, then turn it over and cook until both sides are golden brown and the pancake has risen to about 1cm/½in thick.</p></li>
+                <li><p>Repeat until all the batter is used up. You can keep the pancakes warm in a low oven, but they taste best fresh out the pan.</p></li>
+                <li><p>Serve with lashings of real maple syrup and extra butter, if you like.</p></li>
+            </ol>
+        </div>
+    )
+}
+
+export default RecipePreparation
