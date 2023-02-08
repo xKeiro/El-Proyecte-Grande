62df131b23fd13405f48ab170b64ec35b471db97
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-08 20:46:06 +0100

    Frontend: Refactor Recipe component to us RecipePreparation and RecipeButtons components

--- frontend/src/features/admin/components/Recipe.tsx
+++ frontend/src/features/admin/components/Recipe.tsx
@@ -3,19 +3,25 @@ import RecipeHeader from "./RecipeHeader";
 import RecipeImage from "./RecipeImage";
 import RecipeDescription from "./RecipeDescription";
 import RecipeIngredients from "./RecipeIngredients";
+import RecipePreparation from "@/features/admin/components/RecipePreparation";
+import {RecipeButtons} from "@/features/admin/components/RecipeButtons";
 
 const Recipe = () => {
     let recipe = useLocation().state
     console.log(recipe)
     return (
-        <div className="card card-side bg-base-100 shadow-xl">
-            <RecipeImage />
-            <div className="card-body recipe-body">
-                <RecipeHeader name={recipe.name} cuisine={recipe.cuisine} dishType={recipe.dishType} diets={recipe.diets} mealTimes={recipe.mealTimes} />
-                <RecipeDescription description={recipe.description} />
-                <h3 className="recipe-sub-title">Ingredients</h3>
-                <RecipeIngredients ingredients={recipe.recipeIngredients} />
+        <div className="card shadow-xl">
+            <div className="card card-side bg-base-100">
+                <RecipeImage />
+                <div className="card-body recipe-info">
+                    <RecipeHeader name={recipe.name} cuisine={recipe.cuisine} dishType={recipe.dishType} diets={recipe.diets} mealTimes={recipe.mealTimes} />
+                    <RecipeDescription description={recipe.description} />
+                    <h3 className="recipe-sub-title">Ingredients</h3>
+                    <RecipeIngredients ingredients={recipe.recipeIngredients} />
+                </div>
             </div>
+            <RecipePreparation />
+            <RecipeButtons />
         </div>
     )
 }
