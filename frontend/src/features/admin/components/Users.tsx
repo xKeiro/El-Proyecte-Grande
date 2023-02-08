a6dfc805feb44d040c36350cda00e1f46c2cda77
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-08 16:41:48 +0100

    Frontend: Add Users component

---
+++ frontend/src/features/admin/components/Users.tsx
@@ -0,0 +1,53 @@
+import {getUsers} from "../api/UsersHandler";
+import {useEffect, useState} from "react";
+
+const Users = () => {
+  const [users, setUsers] = useState<any[]>([])
+
+   useEffect(() => {
+     const fetchData = async () => {
+       const result = await getUsers()
+       setUsers(result)
+     }
+     fetchData()
+    }, [])
+
+    return (
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>Username</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users.map(user => (
+                  <tr key={user.id}>
+                    <td>{user.username}</td>
+                    <td>{(user.firstName == null ? "" : user.firstName) + (user.firstName != null && user.lastName != null ? " " : "-") + (user.lastName == null ? "" : user.lastName)}</td>
+                    <td>{user.emailAddress}</td>
+                    <td>
+                        <button className="btn btn-square btn-ghost">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+                        </button>
+                        <button className="btn btn-square btn-ghost">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"></path><polygon points="18 2 22 6 12 16 8 16 8 12 18 2"></polygon></svg>
+                        </button>
+                        <button className="btn btn-square btn-ghost">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+                        </button>
+                    </td>
+                  </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    )
+}
+
+export default Users
