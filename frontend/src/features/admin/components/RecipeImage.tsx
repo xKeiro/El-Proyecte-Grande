51361117a09d57ced7ec7047c6c640634ca07b47
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-08 21:05:35 +0100

    Frontend: Remove figure tag

--- frontend/src/features/admin/components/RecipeImage.tsx
+++ frontend/src/features/admin/components/RecipeImage.tsx
@@ -1,7 +1,7 @@
 
 const RecipeImage = () => {
     return (
-        <figure><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png" alt="Movie"/></figure>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png" alt="Movie"/>
     )
 }
 
