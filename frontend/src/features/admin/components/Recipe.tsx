09f563a5b6fbf442c7826cf2a8201cab94821b43
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-08 17:07:26 +0100

    Frontend: Refactor: use Recipe components

--- frontend/src/features/admin/components/Recipe.tsx
+++ frontend/src/features/admin/components/Recipe.tsx
@@ -1,39 +1,20 @@
 import {useLocation} from "react-router-dom";
+import RecipeHeader from "./RecipeHeader";
+import RecipeImage from "./RecipeImage";
+import RecipeDescription from "./RecipeDescription";
+import RecipeIngredients from "./RecipeIngredients";
 
 const Recipe = () => {
     let recipe = useLocation().state
     console.log(recipe)
     return (
         <div className="card card-side bg-base-100 shadow-xl">
-            <figure><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png" alt="Movie"/></figure>
+            <RecipeImage />
             <div className="card-body recipe-body">
-                <div className="recipe-header">
-                    <div className="recipe-name">{recipe.name}</div>
-                    <div className="badges">
-                        <div className="badge badge-accent">{recipe.cuisine.name}</div>
-                        <div className="badge badge-accent">{recipe.dishType.name}</div>
-                        {
-                            recipe.diets.map((d : any) => (
-                                <div className="badge badge-accent">{d.name}</div>
-                            ))
-                        }
-                        {
-                            recipe.mealTimes.map((m : any) => (
-                                <div className="badge badge-accent">{m.name}</div>
-                            ))
-                        }
-                    </div>
-                </div>
-                <p>{recipe.description}</p>
-
+                <RecipeHeader name={recipe.name} cuisine={recipe.cuisine} dishType={recipe.dishType} diets={recipe.diets} mealTimes={recipe.mealTimes} />
+                <RecipeDescription description={recipe.description} />
                 <h3 className="recipe-sub-title">Ingredients</h3>
-                <ul>
-                {
-                    recipe.recipeIngredients.map((ing : any) => (
-                        <li>{`${ing.amount}${ing.ingredient.unitOfMeasure} ${ing.ingredient.name}`}</li>
-                    ))
-                }
-                </ul>
+                <RecipeIngredients ingredients={recipe.recipeIngredients} />
             </div>
         </div>
     )
