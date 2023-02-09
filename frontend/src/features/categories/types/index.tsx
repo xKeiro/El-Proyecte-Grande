308566c6964fe405756a2c3bb38bbe4f2a690b69
Lilla Meszaros <ameszaroslilla@gmail.com>
2023-02-09 10:41:05 +0100

    Frontend: Fix schemas and types

--- frontend/src/features/categories/types/index.tsx
+++ frontend/src/features/categories/types/index.tsx
@@ -3,7 +3,6 @@ import * as z from 'zod';
 export const categorySchema = z.object({
     id: z.number().int().positive(),
     name: z.string().min(2).max(60),
-    unitOfMeasure: z.string().min(2).max(25),
     })
 
 export type Category = z.infer<typeof categorySchema>
--- frontend/src/features/ingredients/types/index.tsx
+++ frontend/src/features/ingredients/types/index.tsx
@@ -3,7 +3,7 @@ import * as z from 'zod';
 export const ingredientSchema = z.object({
     id: z.number().int().positive(),
     name: z.string().min(2).max(60),
-    unitOfMeasure: z.string().min(2).max(25),
+    unitOfMeasure: z.string().min(1).max(25),
     })
 
 export type Ingredient = z.infer<typeof ingredientSchema>
