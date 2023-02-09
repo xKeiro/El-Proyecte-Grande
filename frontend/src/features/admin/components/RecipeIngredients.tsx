35c82df04db66a03c3e5f4f988b7342e0d0ea5cb
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-09 10:10:03 +0100

    Frontend: Refactor unitOfMeasure rendering

--- frontend/src/features/admin/components/RecipeIngredients.tsx
+++ frontend/src/features/admin/components/RecipeIngredients.tsx
@@ -1,11 +1,18 @@
 
 const RecipeIngredients = (props : any) => {
+    const longUnitOfMeasures = ["bunch", "piece"]
     return (
         <div className="recipe-info grid grid-cols-3">
             <ul>
                 {
                     props.ingredients.map((ing : any) => (
-                        <li key={ing.id}><b><i>{ing.amount + ing.ingredient.unitOfMeasure}</i></b> - {ing.ingredient.name}</li>
+                        <li key={ing.id}>
+                            <b><i>
+                                {ing.amount}
+                                {longUnitOfMeasures.includes(ing.ingredient.unitOfMeasure) ? " " : ""}
+                                {ing.ingredient.unitOfMeasure}
+                            </i></b> - {ing.ingredient.name}
+                        </li>
                     ))
                 }
             </ul>
