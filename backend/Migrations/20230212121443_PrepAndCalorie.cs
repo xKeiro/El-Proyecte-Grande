50d839eefe19aa82fad571bd3328341f9ac7dbe0
xKeiro <nkevin@outlook.hu>
2023-02-12 15:00:35 +0100

    Backend: Add PrepAndCalorie migration

---
+++ backend/Migrations/20230212121443_PrepAndCalorie.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElProyecteGrande.Migrations
+{
+    /// <inheritdoc />
+    public partial class PrepAndCalorie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Recipes",
+                type: "nvarchar(4000)",
+                maxLength: 4000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Difficulty",
+                table: "Recipes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Calorie",
+                table: "Ingredients",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "PreparationSteps",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(750)", maxLength: 750, nullable: false),
+                    Step = table.Column<int>(type: "int", nullable: false),
+                    RecipeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PreparationSteps", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PreparationSteps_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PreparationSteps_RecipeId",
+                table: "PreparationSteps",
+                column: "RecipeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PreparationSteps");
+
+            migrationBuilder.DropColumn(
+                name: "Difficulty",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "Calorie",
+                table: "Ingredients");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Recipes",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4000)",
+                oldMaxLength: 4000);
+        }
+    }
+}
