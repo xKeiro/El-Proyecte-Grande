629c83dabc59dd0571fe7824868662a86a6b369d
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-21 13:43:02 +0100

    Frontend: Change RecipeSearchBox styling

--- frontend/src/features/recipes/components/RecipeSearchBox.tsx
+++ frontend/src/features/recipes/components/RecipeSearchBox.tsx
@@ -1,9 +1,16 @@
 export const RecipeSearchBox = () => {
   return (
+    // <input
+    //   type="search"
+    //   placeholder="Search for a recipe"
+    //   className="text-center text-2xl w-96 h-16 bg-base-300 p-2 rounded-lg drop-shadow-xl transition ease-in-out duration-200 hover:animate-none hover:scale-105"
+    // />
     <input
-      type="search"
-      placeholder="Search for a recipe"
-      className="text-center text-2xl w-96 h-16 bg-base-300 p-2 rounded-lg drop-shadow-xl transition ease-in-out duration-200 hover:animate-none hover:scale-105"
+        type="search"
+        placeholder="Search for a recipe"
+        className="text-center text-xl w-96 h-12 bg-base-300
+                   rounded-box drop-shadow-xl transition ease-in-out duration-200
+                   hover:animate-none hover:scale-105 m-3 p-3"
     />
   );
 };
