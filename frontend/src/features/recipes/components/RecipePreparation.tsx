7bb84e7877f0ae67aecaa0aa36e29249b2c2e86c
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-21 19:05:21 +0100

    Frontend: Styling

--- frontend/src/features/recipes/components/RecipePreparation.tsx
+++ frontend/src/features/recipes/components/RecipePreparation.tsx
@@ -2,7 +2,7 @@ import {TPreparation} from "@/features/recipes";
 
 export const RecipePreparation = (props : {preparationSteps : TPreparation[]}) => {
     return (
-        <div className="card-body recipe-info bg-base-100">
+        <div className="card-body recipe-info bg-base-100 rounded-box">
             <ol className="list-decimal marker:font-bold">
                 {
                     props.preparationSteps.map((prepStep : TPreparation) => (
