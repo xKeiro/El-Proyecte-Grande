1ad3fc3723f9c851df23245fe7db6f69a3beacdc
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-21 13:43:25 +0100

    Frontend: Add search box and pagination to Recipes

--- frontend/src/features/admin/routes/Recipes.tsx
+++ frontend/src/features/admin/routes/Recipes.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { RecipesApi } from '@/features/recipes/api/RecipesApi';
 import { TRecipe } from '@/features/recipes';
+import {RecipeSearchBox} from "@/features/recipes/components/RecipeSearchBox";
 
 export const Recipes = () => {
   const [recipes, setRecipes] = useState<TRecipe[]>([]);
@@ -14,12 +15,21 @@ export const Recipes = () => {
   }, []);
 
   return (
-    <div id="recipes-container" className="grid grid-cols-4 gap-5 text-center py-5">
-      {recipes.map((recipe) => (
-        <Link key={recipe.id} to={`/recipes/${recipe.id}`} state={recipe} className="text-neutral-content hover:underline">
-          {recipe.name}
-        </Link>
+    <div id="recipes-container">
+      <RecipeSearchBox />
+      <div className="grid grid-cols-4 gap-5 text-center py-5">
+        {recipes.map((recipe) => (
+          <Link key={recipe.id} to={`/recipes/${recipe.id}`} state={recipe} className="text-neutral-content hover:underline">
+            {recipe.name}
+          </Link>
       ))}
+      </div>
+      <div className="btn-group w-full justify-center mt-8">
+        <button className="btn btn-md">1</button>
+        <button className="btn btn-md">2</button>
+        <button className="btn btn-md">3</button>
+        <button className="btn btn-md">4</button>
+      </div>
     </div>
   );
 };
