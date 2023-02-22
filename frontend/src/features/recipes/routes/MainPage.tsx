5df34edca63fcb18c833678c6c2f953caf8123b6
Balázs Alberti <alberti.balazs1986@gmail.com>
2023-02-22 10:28:11 +0100

    Frontend: Update search button visual

--- frontend/src/features/recipes/routes/MainPage.tsx
+++ frontend/src/features/recipes/routes/MainPage.tsx
@@ -6,7 +6,9 @@ export const MainPage = () => {
   <div className="drawer">
     <input id="my-drawer" type="checkbox" className="drawer-toggle" />
     <div className="drawer-content">
-      <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Seacrh options</label>
+      <label htmlFor="my-drawer" className="btn drawer-button btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" className='h-7 w-7' viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+      </label>
     </div> 
     <div className="drawer-side">
       <label htmlFor="my-drawer" className="drawer-overlay"></label>
