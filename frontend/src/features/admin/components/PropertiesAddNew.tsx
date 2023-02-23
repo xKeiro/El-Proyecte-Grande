b099176f5d79ee7e05e9073f2665ffcfc9ceb0f7
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-23 15:15:06 +0100

    Frontend: Add option to create new categories and ingredient

--- frontend/src/features/admin/components/PropertiesAddNew.tsx
+++ frontend/src/features/admin/components/PropertiesAddNew.tsx
@@ -1,10 +1,63 @@
+import React, {useState} from "react";
+import { CategoryApi } from "@/features/categories/api/CategoryApi";
+import { IngredientsApi } from "@/features/ingredients/api/IngredientsApi";
 import { CategoriesEnum } from "@/features/categories";
-import {useState} from "react";
+
 export const PropertiesAddNew = () => {
     const [isIngredient, setIsIngredient] = useState<boolean>(false);
+    const [category, setCategory] = useState<CategoriesEnum>(CategoriesEnum.Cuisines);
+    const [responseMsg, setResponseMsg] = useState<string>();
+    const [responseMsgColor, setResponseMsgColor] = useState<string>();
 
     const handleChange = (prop : string) => {
         setIsIngredient(prop == "Ingredients");
+        if (prop != "Ingredients") setCategory(prop as CategoriesEnum);
+        setResponseMsg("");
+        (document.getElementById("newName") as HTMLInputElement).value = "";
+    }
+
+    const sendData = async () => {
+        const inputFields = Array.from(document.querySelectorAll("input"));
+        if (inputFields.every(inp => inp.value != "")) {
+            if (inputFields[0].value.length >= 2) {
+                if (+inputFields[2].value >= 0) {
+                    const newName = (document.getElementById("newName") as HTMLInputElement).value;
+                    let unitOfMeasure;
+                    let calorie;
+                    let res : any;
+
+                    if (isIngredient) {
+                        unitOfMeasure = (document.getElementById("unitOfMeasure") as HTMLInputElement).value;
+                        calorie = +(document.getElementById("calorie") as HTMLInputElement).value;
+
+                        res = await IngredientsApi.add(newName, unitOfMeasure, calorie);
+                    } else {
+                        res = await CategoryApi.add(category, newName);
+                    }
+
+                    if (typeof res == "object") {
+                        setResponseMsgColor("text-success");
+                        setResponseMsg("Successfully created!");
+                    }
+                    else {
+                        setResponseMsgColor("text-error");
+                        setResponseMsg(res);
+                    }
+                }
+                else {
+                    setResponseMsgColor("text-error");
+                    setResponseMsg("Calorie can't be a negative number!");
+                }
+            }
+            else {
+                setResponseMsgColor("text-error");
+                setResponseMsg("Name must be min 2 characters long!");
+            }
+        }
+        else {
+            setResponseMsgColor("text-error");
+            setResponseMsg("All fields are required!");
+        }
     }
 
     return (
@@ -20,29 +73,30 @@ export const PropertiesAddNew = () => {
                 {
                     !isIngredient &&
                     <label className="input-group input-group-md">
-                        <span>Name</span>
-                        <input type="text" placeholder="Type here" minLength={2} maxLength={20} className="input input-bordered input-md" />
+                        <span>Name<span className="text-error px-0 ml-2">*</span></span>
+                        <input id="newName" type="text" placeholder="Type here" minLength={2} maxLength={20} className="input input-bordered input-md" />
                     </label>
                 }
                 {
                     isIngredient &&
                     <div className="flex flex-col gap-4">
                         <label className="input-group input-group-md">
-                            <span>Name</span>
-                            <input type="text" placeholder="Type here" minLength={2} maxLength={60} className="input input-bordered input-md" />
+                            <span>Name<span className="text-error px-0 ml-2">*</span></span>
+                            <input id="newName" type="text" placeholder="Type here" minLength={2} maxLength={60} className="input input-bordered input-md" />
                         </label>
                         <label className="input-group input-group-md">
-                            <span>Unit of Measure</span>
-                            <input type="text" placeholder="Etc.: g, dl, pcs" minLength={1} maxLength={25} className="input input-bordered input-md" />
+                            <span>Unit of Measure<span className="text-error px-0 ml-2">*</span></span>
+                            <input id="unitOfMeasure" type="text" placeholder="Etc.: g, dl, pcs" minLength={1} maxLength={25} className="input input-bordered input-md" />
                         </label>
                         <label className="input-group input-group-md">
-                            <span>Calorie</span>
-                            <input type="number" placeholder="Cal" className="input input-bordered input-md" />
+                            <span>Calorie<span className="text-error px-0 ml-2">*</span></span>
+                            <input id="calorie" type="number" placeholder="Cal" min={0} className="input input-bordered input-md" />
                         </label>
                         <span className="label-text-alt">Cal = kcal / 1000</span>
                     </div>
                 }
-                <button className="btn btn-primary mt-5 w-1/4">Add</button>
+                <button className="btn btn-primary mt-5 w-1/4" onClick={sendData}>Add</button>
+                <span className={"font-bold mt-5 " + responseMsgColor}>{responseMsg}</span>
             </div>
         </div>
     );
