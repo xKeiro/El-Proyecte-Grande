cbdffbdaaf9f68e1c99f72e26d2cc6f08b691450
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-23 18:02:29 +0100

    Frontend: Refactor ingredients list

--- frontend/src/features/recipes/components/RecipeIngredients.tsx
+++ frontend/src/features/recipes/components/RecipeIngredients.tsx
@@ -3,19 +3,17 @@ import {TRecipeIngredient} from "@/features/recipes";
 export const RecipeIngredients = (props : {ingredients : TRecipeIngredient[]}) => {
     const longUnitOfMeasures = ["bunch", "piece"]
     return (
-        <div className="recipe-info grid grid-cols-3">
-            <ul className="list-disc mt-3 ml-5">
-                {
-                    props.ingredients.map((ing : TRecipeIngredient) => (
-                        <li className="font-bold" key={ing.id}>
-                            {ing.amount}
-                            {longUnitOfMeasures.includes(ing.ingredient.unitOfMeasure) ? " " : ""}
-                            {ing.ingredient.unitOfMeasure}
-                            <span className="text-primary pl-2">{ing.ingredient.name}</span>
-                        </li>
-                    ))
-                }
-            </ul>
+        <div className="recipe-info grid grid-cols-2 mt-3">
+            {
+                props.ingredients.map((ing : TRecipeIngredient) => (
+                    <li className="font-bold ml-5" key={ing.id}>
+                        {ing.amount}
+                        {longUnitOfMeasures.includes(ing.ingredient.unitOfMeasure) ? " " : ""}
+                        {ing.ingredient.unitOfMeasure}
+                        <span className="text-primary pl-2">{ing.ingredient.name}</span>
+                    </li>
+                ))
+            }
         </div>
     )
 }
