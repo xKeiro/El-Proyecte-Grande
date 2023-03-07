0a2778c0637528c771d6994edbff99efb14fd5ca
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-03-07 20:34:56 +0100

    Backend: Add JwtService

---
+++ backend/Interfaces/Services/IJwtService.cs
@@ -0,0 +1,12 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using backend.Dtos.Users.User;
+
+namespace backend.Interfaces.Services
+{
+    public interface IJwtService
+    {
+        //string Generate(int id);
+        //JwtSecurityToken Verify(string jwt);
+        string Authenticate(UserPublic user);
+    }
+}
