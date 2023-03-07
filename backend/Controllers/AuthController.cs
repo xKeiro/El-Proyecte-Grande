6786b3c068fc09b8693814e216d4f4a1536cef7e
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-03-07 20:38:21 +0100

    Backend: Update endpoints with implementation and authorization

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using backend.Dtos.Users.User;
 using backend.Interfaces.Services;
 using backend.Models;
 using backend.Models.Users;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers
@@ -14,19 +15,22 @@ namespace backend.Controllers
         private readonly IUserService<UserPublic, UserWithoutId> _service;
         private readonly IMapper _mapper;
         private readonly IStatusMessageService<User> _statusMessage;
+        private readonly IJwtService _jwtService;
 
-        public AuthController(IUserService<UserPublic, UserWithoutId> service, IMapper mapper, IStatusMessageService<User> statusMessage)
+        public AuthController(IUserService<UserPublic, UserWithoutId> service, IMapper mapper, IStatusMessageService<User> statusMessage, IJwtService jwtService)
         {
             _service = service;
             _mapper = mapper;
             _statusMessage = statusMessage;
+            _jwtService = jwtService;
         }
 
+        [AllowAnonymous]
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusMessage))]
         [ProducesResponseType(StatusCodes.Status409Conflict, Type = typeof(StatusMessage))]
-        public async Task<IActionResult> Register([FromForm] UserRegister newUser)
+        public async Task<IActionResult> Register(UserRegister newUser)
         {
             var user = _mapper.Map<UserRegister, UserWithoutId>(newUser);
 
@@ -36,20 +40,41 @@ namespace backend.Controllers
             return StatusCode(StatusCodes.Status201Created, user);
         }
 
+        [AllowAnonymous]
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> Login([FromForm] UserLogin user)
+        public async Task<IActionResult> Login(UserLogin user)
         {
-            if (!await _service.FindForLogin(user)) return StatusCode(StatusCodes.Status400BadRequest, "Invalid credentials!");
+            UserPublic resUser = await _service.FindForLogin(user);
+            if (resUser is null) return StatusCode(StatusCodes.Status400BadRequest, new { message = "Invalid credentials!" });
+
+            //string jwt = _jwtService.Generate(resUser.Id);
+            string jwt = _jwtService.Authenticate(resUser);
+
+            Response.Cookies.Append("jwt", jwt, new CookieOptions
+            {
+                HttpOnly = true
+            });
+
+            Response.Cookies.Append("username", resUser.Username, new CookieOptions
+            {
+                HttpOnly = true
+            });
 
             return StatusCode(StatusCodes.Status200OK);
         }
 
-        [HttpPost]
+        [Authorize]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public IActionResult Logout()
         {
-            return Ok("logout");
+            Response.Cookies.Delete("jwt");
+            Response.Cookies.Delete("username");
+
+            return Ok("success");
         }
     }
 }
