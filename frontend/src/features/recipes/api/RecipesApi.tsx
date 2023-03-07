28b5f84095a0196f063836ec3b2d7781450bb990
xKeiro <nkevin@outlook.hu>
2023-03-07 08:11:43 +0100

    Frontend: Add filter for not owned ingredients

--- frontend/src/features/recipes/api/RecipesApi.tsx
+++ frontend/src/features/recipes/api/RecipesApi.tsx
@@ -28,14 +28,15 @@ export abstract class RecipesApi {
     await axios.delete(`${API_URL}/Recipes/${id}`);
   }
 
-  public static async filterRecipes(cuisineIds: number[], dietIds: number[], mealTimeIds: number[], dishTypeIds: number[], ingredientIds: number[], name: string) {
+  public static async filterRecipes(cuisineIds: number[], dietIds: number[], mealTimeIds: number[], dishTypeIds: number[], ingredientIds: number[], name: string, maxNumberOfNotOwnedIngredients: number) {
     const ingredientParams = ingredientIds.length > 0 ? ingredientIds.map(id => `IngredientIds=${id}`).join('&') : '';
     const cuisineParams = cuisineIds.length > 0 ? cuisineIds.map(id => `CuisineIds=${id}`).join('&') : '';
     const dietParams = dietIds.length > 0 ? dietIds.map(id => `DietIds=${id}`).join('&') : '';
     const mealTimeParams = mealTimeIds.length > 0 ? mealTimeIds.map(id => `MealTimeIds=${id}`).join('&') : '';
     const dishTypeParams = dishTypeIds.length > 0 ? dishTypeIds.map(id => `DishTypeIds=${id}`).join('&') : '';
     const nameParam = name.length > 0 ? `Name=${name}` : '';
-    const apiUrl = `${API_URL}/Recipes?${nameParam}${ingredientParams}&${cuisineParams}&${mealTimeParams}&${dietParams}&${dishTypeParams}`;
+    const maxNumberOfNotOwnedIngredientsParam = maxNumberOfNotOwnedIngredients > 0 ? `MaxNumberOfNotOwnedIngredients=${maxNumberOfNotOwnedIngredients}` : '';
+    const apiUrl = `${API_URL}/Recipes?${nameParam}${ingredientParams}&${cuisineParams}&${mealTimeParams}&${dietParams}&${dishTypeParams}${maxNumberOfNotOwnedIngredientsParam}`;
     const response = await axios.get(apiUrl);
     return response.data;
   }
