f9a123bc4882b0dd52992507299bd8ae9fdc8391
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-03-08 16:18:41 +0100

    Frontend: Add OptionLink component to layout

---
+++ frontend/src/components/Layout/OptionLink.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import {API_URL} from "@/config";
+
+export const OptionLink = ({text, to, isLogout} : {text : string, to : string, isLogout : boolean}) => {
+    const logout = async () => {
+        await fetch(API_URL + "/Auth/Logout", {
+            method: "POST",
+            headers: {"Content-type": "application/json"},
+            credentials: "include",
+        });
+    }
+
+    if (!isLogout) return (
+         <Link to={to} className="text-xl">
+            {text}
+        </Link>
+    );
+    return (
+        <Link to={to} onClick={logout} className="text-xl">
+            {text}
+        </Link>
+    )
+}
