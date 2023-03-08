5c5ae7aca69f697434f33f3e9c70970eafeebb89
Lilla Meszaros <ameszaroslilla@gmail.com>
2023-03-08 21:32:54 +0100

    Frontend: Add Add New Recipe button to admin recipes

--- frontend/src/features/admin/routes/Recipes.tsx
+++ frontend/src/features/admin/routes/Recipes.tsx
@@ -3,6 +3,7 @@ import { Link, Navigate } from 'react-router-dom';
 import { RecipesApi } from '@/features/recipes/api/RecipesApi';
 import { TRecipe } from '@/features/recipes';
 import { RecipeSearchBox } from "@/features/recipes/components/RecipeSearchBox";
+import { RecipeAddNew } from '../components/RecipeAddNew';
 
 export const Recipes = () => {
   const [recipes, setRecipes] = useState<TRecipe[]>([]);
@@ -18,29 +19,33 @@ export const Recipes = () => {
     fetchData();
   }, []);
 
-  const handleSearch = async (name : string = "") => {
-      const recipesList = await RecipesApi.filterRecipes([], [], [], [], [], name);
-      setRecipes(recipesList);
+  const handleSearch = async (name: string = "") => {
+    const recipesList = await RecipesApi.filterRecipes([], [], [], [], [], name);
+    setRecipes(recipesList);
   }
 
   if (isAdmin)
-  return (
-    <div id="recipes-container" className="text-base-content">
-      <RecipeSearchBox searchTerm={searchTerm} setSearchTerm={setSearchTerm} handleRecipeSearch={handleSearch} />
-      <div className="grid grid-cols-4 gap-5 text-center py-5">
-        {recipes.map((recipe) => (
-          <Link key={recipe.id} to={`/admin/recipes/${recipe.id}`} className="hover:underline">
-            {recipe.name}
-          </Link>
-      ))}
+    return (
+      <div id="recipes-container" className="text-base-content">
+        <RecipeSearchBox searchTerm={searchTerm} setSearchTerm={setSearchTerm} handleRecipeSearch={handleSearch} />
+        <div className="grid grid-cols-4 gap-5 text-center py-5">
+          {recipes.map((recipe) => (
+            <Link key={recipe.id} to={`/admin/recipes/${recipe.id}`} className="hover:underline">
+              {recipe.name}
+            </Link>
+          ))}
+        </div>
+        <div className='flex justify-center mt-4'>
+          <Link to="/admin/recipes/add" className="btn btn-primary">Add New Recipe</Link>
+        </div>
+
+        <div className="btn-group w-full justify-center mt-8">
+          <button className="btn btn-md">1</button>
+          <button className="btn btn-md">2</button>
+          <button className="btn btn-md">3</button>
+          <button className="btn btn-md">4</button>
+        </div>
       </div>
-      <div className="btn-group w-full justify-center mt-8">
-        <button className="btn btn-md">1</button>
-        <button className="btn btn-md">2</button>
-        <button className="btn btn-md">3</button>
-        <button className="btn btn-md">4</button>
-      </div>
-    </div>
-  );
+    );
   return (<Navigate to="/unauthorized" />)
 };
