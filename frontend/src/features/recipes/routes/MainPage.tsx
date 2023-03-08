fbe96d3e0cffee4a01fd17c9ed0f9383fd80e5ee
xKeiro <nkevin@outlook.hu>
2023-03-08 16:44:22 +0100

    Frontend: Add scroll to the top button to the main page

--- frontend/src/features/recipes/routes/MainPage.tsx
+++ frontend/src/features/recipes/routes/MainPage.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { createElement, useEffect, useRef, useState } from 'react';
 import { RecipeFilter } from '../components/RecipeFilter';
 import { TRecipe } from '../types';
 import { FilteredRecipe } from '../components/FilteredRecipe';
@@ -7,6 +7,7 @@ export const MainPage = () => {
 
   const [showFilteredRecipe, setShowFilteredRecipe] = useState(false);
   const [filteredRecipes, setFilteredRecipes] = useState<TRecipe[]>([]);
+  const [showToTopButton, setShowToTopButton] = useState(false);
   const firstTime = useRef<Boolean>(true);
 
   function handleData(data: TRecipe[]) {
@@ -21,6 +22,21 @@ export const MainPage = () => {
       element?.scrollIntoView({ behavior: 'smooth', });
     }
   }
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        setShowToTopButton(true);
+      } else {
+        setShowToTopButton(false);
+      }
+    });
+  }, []);
 
   return (
     <div>
@@ -34,6 +50,7 @@ export const MainPage = () => {
             : <div className="text-center">Loading the recipes...</div>}
         </div>
       </div>
+      {showToTopButton && (<button className='btn btn-circle fixed right-11 bottom-20 w-16 h-16 text-2xl text-base-content outline-base-content glass' type='button' title='Jump back to the top of the page' onClick={scrollToTop}><svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 19V6M5 12l7-7 7 7"/></svg></button>)}
     </div>
   );
 };
