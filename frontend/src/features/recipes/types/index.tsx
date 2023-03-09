e67e10970eaacff6c21773a7c2d9f923eba6d385
Lilla Meszaros <ameszaroslilla@gmail.com>
2023-03-09 10:42:31 +0100

    Frontend: Update Recipe types

--- frontend/src/features/recipes/types/index.tsx
+++ frontend/src/features/recipes/types/index.tsx
@@ -1,6 +1,12 @@
 import { ingredientSchema } from '@/features/ingredients';
 import { categorySchema } from '@/features/categories';
-import * as z from 'zod'; 
+import * as z from 'zod';
+
+export enum Difficulty {
+    Easy = 'Easy',
+    Medium = 'Medium',
+    Hard = 'Hard',
+}
 
 export const recipeIngredientSchema = z.object({
     id: z.number().int().positive(),
@@ -13,26 +19,19 @@ export const preparationSchema = z.object({
     description: z.string().min(2).max(750),
 })
 
+export const recipeIngredientToPost = z.object({
+    ingredientId: z.number().int().positive(),
+    amount: z.number().positive()
+});
+
+
 export type TPreparation = z.infer<typeof preparationSchema>
-
-export const recipesSchema =z.array(z.object({
-    id: z.number().int().positive(),
-    name: z.string().min(2).max(120),
-    description: z.string(),
-    recipeIngredients: z.array(recipeIngredientSchema),
-    cuisine: categorySchema,
-    mealTimes: z.array(categorySchema),
-    diets: z.array(categorySchema),
-    dishType: categorySchema,
-    preparationSteps: z.array(preparationSchema)
-}))
-
-export type TRecipeIngredient = z.infer<typeof recipeIngredientSchema>;
 
 export const recipeSchema = z.object({
     id: z.number().int().positive(),
     name: z.string().min(2).max(120),
     description: z.string(),
+    difficulty: z.nativeEnum(Difficulty),
     recipeIngredients: z.array(recipeIngredientSchema),
     cuisine: categorySchema,
     mealTimes: z.array(categorySchema),
@@ -42,3 +41,12 @@ export const recipeSchema = z.object({
 });
 
 export type TRecipe = z.infer<typeof recipeSchema>;
+
+export const recipesSchema = z.array(recipeSchema);
+
+export type TRecipeIngredient = z.infer<typeof recipeIngredientSchema>;
+
+export type RecipeIngredientToPost = z.infer<typeof recipeIngredientToPost>;
+
+
+
