4bf555306cec5236c17b1612a9ba9ebc987c6cb4
xKeiro <nkevin@outlook.hu>
2023-03-09 19:41:53 +0100

    Frontend: Update admin/recipes search box location

--- frontend/src/features/admin/routes/Recipes.tsx
+++ frontend/src/features/admin/routes/Recipes.tsx
@@ -4,7 +4,7 @@ import { RecipesApi } from '@/features/recipes/api/RecipesApi';
 import { TRecipe, TRecipesFilter, TRecipesWithPagination } from '@/features/recipes';
 import { RecipeSearchBox } from "@/features/recipes/components/RecipeSearchBox";
 
-export const Recipes = ({ isAdmin } : { isAdmin : boolean }) => {
+export const Recipes = ({ isAdmin }: { isAdmin: boolean }) => {
   const [recipesWithPagination, setRecipesWithPagination] = useState<TRecipesWithPagination | null>(null);
   const [searchTerm, setSearchTerm] = useState<string>('');
 
@@ -15,7 +15,7 @@ export const Recipes = ({ isAdmin } : { isAdmin : boolean }) => {
     fetchData();
   }, []);
 
-  const handleSearch = async (searchString : string = "") => {
+  const handleSearch = async (searchString: string = "") => {
     const filter: TRecipesFilter = {
       cuisineIds: [],
       dietIds: [],
@@ -33,23 +33,25 @@ export const Recipes = ({ isAdmin } : { isAdmin : boolean }) => {
   }
 
   if (isAdmin)
-  return (
-    <div id="recipes-container" className="text-base-content">
-      <RecipeSearchBox searchTerm={searchTerm} setSearchTerm={setSearchTerm} handleRecipeSearch={handleSearch} />
-      <div className="grid grid-cols-4 gap-5 text-center py-5">
-        {recipesWithPagination?.recipes.map((recipe) => (
-          <Link key={recipe.id} to={`/admin/recipes/${recipe.id}`} className="hover:underline">
-            {recipe.name}
-          </Link>
-      ))}
+    return (
+      <div id="recipes-container" className="text-base-content">
+        <div className='text-center'>
+          <RecipeSearchBox searchTerm={searchTerm} setSearchTerm={setSearchTerm} handleRecipeSearch={handleSearch} />
+        </div>
+        <div className="grid grid-cols-4 gap-5 text-center py-5">
+          {recipesWithPagination?.recipes.map((recipe) => (
+            <Link key={recipe.id} to={`/admin/recipes/${recipe.id}`} className="hover:underline">
+              {recipe.name}
+            </Link>
+          ))}
+        </div>
+        <div className="btn-group w-full justify-center mt-8">
+          <button className="btn btn-md">1</button>
+          <button className="btn btn-md">2</button>
+          <button className="btn btn-md">3</button>
+          <button className="btn btn-md">4</button>
+        </div>
       </div>
-      <div className="btn-group w-full justify-center mt-8">
-        <button className="btn btn-md">1</button>
-        <button className="btn btn-md">2</button>
-        <button className="btn btn-md">3</button>
-        <button className="btn btn-md">4</button>
-      </div>
-    </div>
-  );
+    );
   return (<Navigate to="/unauthorized" />)
 };
