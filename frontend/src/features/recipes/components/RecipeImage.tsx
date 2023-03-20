e361515615f30bfd221df13459b8092c1782e573
Lilla Meszaros <ameszaroslilla@gmail.com>
2023-03-20 22:05:19 +0100

    Frontend: Update RecipeImage to images from backend

--- frontend/src/features/recipes/components/RecipeImage.tsx
+++ frontend/src/features/recipes/components/RecipeImage.tsx
@@ -1,8 +1,23 @@
+import { API_URL } from "@/config";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export const RecipeImage = (props: { id: number, name : string, size?: { width: number, height: number } }) => {
-    let path = "/src/assets/images/recipes/";
+    const [imageData, setImageData] = useState("");
+
+    useEffect(() => {
+        axios.get(`${API_URL}/ImageUpload/${props.id}`, { responseType: 'blob' })
+          .then(response => {
+            const imageUrl = URL.createObjectURL(response.data);
+            setImageData(imageUrl);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }, [props.id]);
+
     let { width, height } = props.size || { width: 400, height: 500 };
     return (
-        <img className="rounded-lg" src={path + props.id + ".jpg"} alt={props.name} title={props.name} width={width} height={height}/>
+        <img className="rounded-lg" src={imageData} alt={props.name} title={props.name} width={width} height={height}/>
     )
 }
