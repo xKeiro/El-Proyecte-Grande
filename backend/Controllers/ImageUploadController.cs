20d053ddc53c81cd85c015623fe5e3ee43f0fdcc
Lilla Meszaros <ameszaroslilla@gmail.com>
2023-03-20 22:03:07 +0100

    Backend: Refactor ImageUpload controller

--- backend/Controllers/ImageUploadController.cs
+++ backend/Controllers/ImageUploadController.cs
@@ -14,23 +14,21 @@ namespace backend.Controllers
         {
             try
             {
-                // 1. get the file form the request
                 var postedFile = Request.Form.Files[0];
-                // 2. set the file uploaded folder
                 var uploadFolder = Path.Combine(Directory.GetCurrentDirectory(), "UploadedImages");
-                // 3. check for the file length, if it is more than 0 the save it
                 if (postedFile.Length > 0)
                 {
-                    // 3a. read the file name of the received file
-                    var fileName = ContentDispositionHeaderValue.Parse(postedFile.ContentDisposition)
-                        .FileName.Trim('"');
-                    // 3b. save the file on Path
-                    var finalPath = Path.Combine(uploadFolder, fileName);
-                    using (var fileStream = new FileStream(finalPath, FileMode.Create))
+                    var fileName = ContentDispositionHeaderValue.Parse(postedFile.ContentDisposition).FileName?.Trim('"');
+                    if (fileName != null)
                     {
-                        postedFile.CopyTo(fileStream);
+                        var finalPath = Path.Combine(uploadFolder, fileName);
+                        using (var fileStream = new FileStream(finalPath, FileMode.Create))
+                        {
+                            postedFile.CopyTo(fileStream);
+                        }
+                        return Ok($"File is uploaded Successfully");
                     }
-                    return Ok($"File is uploaded Successfully");
+                    return BadRequest("The File name can not be null.");
                 }
                 else
                 {
@@ -45,13 +43,11 @@ namespace backend.Controllers
             }
         }
 
-        [HttpGet("{id}")] public async Task<IActionResult> ShowImage(string id)
+        [HttpGet("{id}")]
+        public async Task<IActionResult> ShowImage(string id)
         {
-            // Construct the path to the image file based on the ID.
             string imagePath = $"/DRIVERS/CODECOOL/ADVANCE/0_TW/El-Proyecte-Grande/backend/UploadedImages/{id}.jpg";
-            // Read the image data from the file.
             byte[] imageData = await System.IO.File.ReadAllBytesAsync(imagePath);
-            // Return the image data as a file stream result.
             return File(imageData, "image/jpeg");
         }
     }
