cb06f6a2cfdcd9bcc84a15bea32d5a30e3de86e7
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-03-21 14:15:42 +0100

    Frontend: Fix login

--- frontend/src/features/auth/routes/Login.tsx
+++ frontend/src/features/auth/routes/Login.tsx
@@ -30,22 +30,30 @@ export const Login = ({ loggedInUsername, setLoggedInUsername, setIsAdmin } : {
       });
       const result = await response.json();
 
-      sessionStorage.setItem("username", result.username);
-      sessionStorage.setItem("isAdmin", result.isAdmin);
-      setLoggedInUsername(result.username);
-      setIsAdmin(result.isAdmin);
-
       if (result.hasOwnProperty("message")) {
         setHideErrorMsg(false);
         setErrorMsg("Invalid credentials!");
       }
-      else navigate("/");
+      else {
+        sessionStorage.setItem("username", result.username);
+        sessionStorage.setItem("isAdmin", result.isAdmin);
+        setLoggedInUsername(result.username);
+        setIsAdmin(result.isAdmin);
+
+        navigate("/");
+      }
     }
     else {
       setHideErrorMsg(false);
       if (username == "" || password == "") setErrorMsg("Every field is required!");
       else setErrorMsg("Username is too short!");
     }
+  }
+
+  const googleLogin = async (e : SyntheticEvent) => {
+    e.preventDefault();
+
+    const response = await fetch(API_URL + "/Auth/GoogleLogin");
   }
 
   if (loggedInUsername == null) return (
@@ -74,6 +82,24 @@ export const Login = ({ loggedInUsername, setLoggedInUsername, setIsAdmin } : {
                 </div>
                 <div className="form-control mt-6">
                   <button className="btn btn-primary" onClick={submit}>Login</button>
+                </div>
+                <div className="mt-2">
+                  <button
+                      className="btn w-full text-white bg-[#4285F4] border-[#4285F4] hover:border-[#4285F4]/90 hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-inherit text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55"
+                      onClick={googleLogin}>
+                    <svg
+                        className="w-4 h-4 mr-2 -ml-1"
+                        aria-hidden="true"
+                        focusable="false"
+                        data-prefix="fab"
+                        data-icon="google"
+                        role="img"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 488 512">
+                      <path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path>
+                    </svg>
+                    Sign in with Google
+                  </button>
                 </div>
               <div id="cred-alert" className="mt-4 alert alert-error shadow-lg justify-center text-lg font-bold" hidden={hideErrorMsg}>
                   <span>{errorMsg}</span>
