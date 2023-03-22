953a92ec3f70aab87f8c0c9fe51ba09a9933e7bb
BalazsAlberti <101410064+BalazsAlberti@users.noreply.github.com>
2023-03-22 08:59:50 +0100

    Frontend: Update recipe cards column responsibility

--- frontend/src/features/recipes/components/FilteredRecipe.tsx
+++ frontend/src/features/recipes/components/FilteredRecipe.tsx
@@ -6,9 +6,9 @@ export const FilteredRecipe = ({ recipes }: { recipes: TRecipe[] }) => {
   if (!recipes) return (<div>Loading...</div>);
   if (recipes.length === 0) return (<div className="text-center text-xl">No recipes found, please adjust your filters!</div>);
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 justify-center">
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 justify-center">
       {recipes.map(recipe => (
-        <div key={recipe.id} className="card w-72 md:max-2xl:w-96 bg-base-200 shadow-xl mx-auto">
+        <div key={recipe.id} className="card w-72 md:w-96 bg-base-200 shadow-xl mx-auto">
           <figure className="px-10 pt-10 rounded-x">
             <RecipeImage id={recipe.id} name={recipe.name} size={{ width: 200, height: 250 }} />
           </figure>
