2b1ac0e3f3354559ba7d937782a7381654c2b590
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-04-04 10:04:03 +0200

    Backend: Remove Equal override from DTO

--- backend/Dtos/Users/User/UserPublic.cs
+++ backend/Dtos/Users/User/UserPublic.cs
@@ -3,14 +3,4 @@
 public class UserPublic : UserBaseDetails
 {
     public required int Id { get; set; }
-
-    public override bool Equals(object? obj)
-    {
-        if (obj != null && obj.GetType().Equals(GetType()))
-        {
-            var pubObj = (UserPublic)obj;
-            return pubObj.Id == Id;
-        }
-        return false;
-    }
 }
