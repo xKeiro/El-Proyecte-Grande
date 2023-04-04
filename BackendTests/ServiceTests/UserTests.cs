a24b93d431af044d9a5653184a046dc50c67a032
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-04-04 10:04:36 +0200

    Backend: Refactor setup and test

--- BackendTests/ServiceTests/UserTests.cs
+++ BackendTests/ServiceTests/UserTests.cs
@@ -1,6 +1,5 @@
 using AutoMapper;
 using backend.Dtos.Users.User;
-using backend.Maps;
 using backend.Models.Users;
 using backend.Services;
 using backend.Services.Users;
@@ -10,12 +9,14 @@ using Moq.EntityFrameworkCore;
 
 namespace BackendTests.ServiceTests
 {
+    [TestFixture]
     public class Tests
     {
         private Mock<ElProyecteGrandeContext> _mockContext;
         private UserService _service;
         private List<User> _users;
-        private IMapper _mapper;
+        private List<UserPublic> _usersPublic;
+        private Mock<IMapper> _mapper;
 
         [SetUp]
         public void Setup()
@@ -53,23 +54,50 @@ namespace BackendTests.ServiceTests
                     Password = "98765"
                 }
             };
+
+            _usersPublic = new List<UserPublic>()
+            {
+                new ()
+                {
+                    Id = 1,
+                    Username = "longdon",
+                    EmailAddress = "ding@dong.com",
+                    FirstName = null,
+                    LastName = null,
+                    IsAdmin = false,
+                },
+                new ()
+                {
+                    Id = 2,
+                    Username = "whiskers",
+                    EmailAddress = "why@you.net",
+                    FirstName = "John",
+                    LastName = "Faruga",
+                    IsAdmin = false,
+                },
+                new ()
+                {
+                    Id = 3,
+                    Username = "omegalol",
+                    EmailAddress = "super@nice.com",
+                    FirstName = null,
+                    LastName = null,
+                    IsAdmin = true,
+                }
+            };
             _mockContext = new(new DbContextOptions<ElProyecteGrandeContext>());
             _mockContext.Setup(x => x.Users).ReturnsDbSet(_users);
 
-            var mappingConfig = new MapperConfiguration(mc =>
-            {
-                mc.AddProfile(typeof(MappingProfile));
-            });
-            _mapper = mappingConfig.CreateMapper();
+            _mapper = new Mock<IMapper>();
+            _mapper.Setup(m => m.Map<List<User>, List<UserPublic>>(It.IsAny<List<User>>())).Returns(_usersPublic);
 
-            _service = new UserService(_mockContext.Object, _mapper);
+            _service = new UserService(_mockContext.Object, _mapper.Object);
         }
 
         [Test]
         public async Task GetAll_WhenCalled_ReturnsUserList()
         {
-            var expectedUsers = _mapper.Map<List<User>, List<UserPublic>>(_users);
-            CollectionAssert.AreEqual(expectedUsers, await _service.GetAll());
+            CollectionAssert.AreEqual(_usersPublic, await _service.GetAll());
         }
     }
 }
