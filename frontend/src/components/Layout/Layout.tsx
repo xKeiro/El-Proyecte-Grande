459e7dec2cb7e536a00e7c9719848b8d77fc4f8b
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-04-06 11:04:12 +0200

    Frontend: Fix project errors

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,12 +1,16 @@
 import { Footer } from "./Footer";
 import { NavBar } from "./NavBar";
-import { Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
-export const Layout = () => {
+export const Layout = ({ username, isAdmin, setUsername, setIsAdmin } : {
+    username : string | null,
+    isAdmin : boolean,
+    setUsername : React.Dispatch<React.SetStateAction<string | null>>,
+    setIsAdmin : React.Dispatch<React.SetStateAction<boolean>> }) => {
     return (
         <div>
-            <NavBar />
+            <NavBar isAdmin={isAdmin} username={username} setIsAdmin={setIsAdmin} setUsername={setUsername} />
             <Outlet />
             <Footer />
         </div>
