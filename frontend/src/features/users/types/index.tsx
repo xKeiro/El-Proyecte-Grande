c7b3e55b5b4245ceaa62c94279c9183ece659ea4
xKeiro <nkevin@outlook.hu>
2023-05-11 17:26:48 +0200

    Frontend: Fix validation issue for user first and lastname

--- frontend/src/features/users/types/index.tsx
+++ frontend/src/features/users/types/index.tsx
@@ -22,8 +22,8 @@ export const userSchema = z.object({
     id: z.number().int().positive(),
     username: z.string().min(2).max(50),
     emailAddress: z.string().email(),
-    firstName: z.string().min(2).max(100).nullable(),
-    lastName: z.string().min(2).max(100).nullable(),
+    firstName: z.string().max(100).nullable(),
+    lastName: z.string().max(100).nullable(),
     isAdmin: z.boolean()
 })
 
